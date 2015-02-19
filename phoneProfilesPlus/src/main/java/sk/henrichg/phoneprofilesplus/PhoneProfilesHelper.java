59ebeac842a8a2927fc48d4e6b16ce952d776077
henrichg <henrich.gron@chello.sk>
2015-02-19 22:42:09 +0100

    Updated RootTools

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesHelper.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesHelper.java
@@ -36,7 +36,7 @@ public class PhoneProfilesHelper {
 
 	public static int PPHelperVersion = -1;
 
-	public static final int PPHELPER_CURRENT_VERSION = 35;
+	public static final int PPHELPER_CURRENT_VERSION = 36;
 	
 	private static boolean errorNoRoot = false;
 	private static boolean nowPPHelperUninstalled = false; 
