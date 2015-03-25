df86e961f0806fd602f42c3fbbcff8707fbad48d
henrichg <henrich.gron@chello.sk>
2015-03-25 15:31:43 +0100

    NumberPickerPreference as MaterialDialog

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NumberPickerPreference.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NumberPickerPreference.java
@@ -1,33 +1,17 @@
-/*
- * Copyright (C) 2011 The CyanogenMod Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package sk.henrichg.phoneprofilesplus;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Bundle;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.NumberPicker;
 
-/*
- * @author Danesh
- * @author nebkat
- */
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.util.DialogUtils;
 
 public class NumberPickerPreference extends DialogPreference {
 	
@@ -38,6 +22,8 @@ public class NumberPickerPreference extends DialogPreference {
     private String mMaxExternalKey, mMinExternalKey;
 
     private NumberPicker mNumberPicker;
+
+    private int mColor = 0;
 
     public NumberPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -51,24 +37,38 @@ public class NumberPickerPreference extends DialogPreference {
         mMin = numberPickerType.getInt(R.styleable.NumberPickerPreference_min, 0);
 
         numberPickerType.recycle();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            mColor = DialogUtils.resolveColor(context, R.attr.colorAccent);
     }
 
     @Override
-    protected View onCreateDialogView() {
+    protected void showDialog(Bundle state) {
+        MaterialDialog.Builder mBuilder = new MaterialDialog.Builder(getContext())
+                .title(getDialogTitle())
+                .icon(getDialogIcon())
+                .positiveText(getPositiveButtonText())
+                .negativeText(getNegativeButtonText())
+                .callback(callback)
+                .content(getDialogMessage());
+
+        View layout = LayoutInflater.from(getContext()).inflate(R.layout.activity_number_pref_dialog, null);
+        onBindDialogView(layout);
+
+        /*
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            getEditText().getBackground().setColorFilter(mColor, PorterDuff.Mode.SRC_ATOP);
+        */
 
         // External values
         if (mMaxExternalKey != null) {
             mMax = getSharedPreferences().getInt(mMaxExternalKey, mMax);
         }
         if (mMinExternalKey != null) {
-        	mMin = getSharedPreferences().getInt(mMinExternalKey, mMin);
+            mMin = getSharedPreferences().getInt(mMinExternalKey, mMin);
         }
 
-        LayoutInflater inflater =
-                (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.activity_number_pref_dialog, null);
-
-        mNumberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
+        mNumberPicker = (NumberPicker) layout.findViewById(R.id.number_picker);
 
         // Initialize state
         mNumberPicker.setMaxValue(mMax);
@@ -76,39 +76,32 @@ public class NumberPickerPreference extends DialogPreference {
         mNumberPicker.setValue(Integer.valueOf(value));
         mNumberPicker.setWrapSelectorWheel(false);
 
-        /*
-        mNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int valueOld, int valueNew) {
+        mBuilder.customView(layout, false);
 
-                //Toast.makeText(this, "Value was: " + Integer.toString(valueOld) + " is now: " + Integer.toString(valueNew), Toast.LENGTH_SHORT).show();
-                //persistString(String.valueOf(valueNew));
-            	
-        		callChangeListener(String.valueOf(valueNew));
+        MaterialDialog mDialog = mBuilder.build();
+        if (state != null)
+            mDialog.onRestoreInstanceState(state);
 
-            }
-        });
-        */
-        
-        return view;
+        mDialog.setOnDismissListener(this);
+        mDialog.show();
     }
+
+    private final MaterialDialog.ButtonCallback callback = new MaterialDialog.ButtonCallback() {
+        @Override
+        public void onPositive(MaterialDialog dialog) {
+            mNumberPicker.clearFocus();
+
+            value = String.valueOf(mNumberPicker.getValue());
+
+            if (callChangeListener(value))
+            {
+                //persistInt(mNumberPicker.getValue());
+                persistString(value);
+            }
+        }
+    };
 
     @Override
-    protected void onDialogClosed(boolean positiveResult) {
-        if (positiveResult) {
-        	mNumberPicker.clearFocus();
-
-        	value = String.valueOf(mNumberPicker.getValue());
-        	
-    		if (callChangeListener(value))
-    		{
-	    		//persistInt(mNumberPicker.getValue());
-	            persistString(value);
-    		}
-        }
-    }
-    
-    @Override 
     protected Object onGetDefaultValue(TypedArray ta, int index)
     {
 		super.onGetDefaultValue(ta, index);
