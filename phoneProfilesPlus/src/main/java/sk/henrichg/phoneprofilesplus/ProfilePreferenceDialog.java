fabad474ad891e7e722d057f3b1a2056cdbf5c03
henrichg <henrich.gron@chello.sk>
2015-03-25 14:47:38 +0100

    ProfilePreference as MaterialDialog

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfilePreferenceDialog.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfilePreferenceDialog.java
@@ -12,10 +12,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-//import android.preference.Preference;
-//import android.preference.Preference.OnPreferenceChangeListener;
-
-
 public class ProfilePreferenceDialog
 {
 
