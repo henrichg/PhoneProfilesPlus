7b4b45d928f474177503ae2a985bc63db637c416
henrichg <henrich.gron@chello.sk>
2015-06-02 11:30:23 +0200

    .

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/FirstStartService.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/FirstStartService.java
@@ -104,7 +104,7 @@ public class FirstStartService extends IntentService {
         // startneme eventy
 		if (GlobalData.getGlobalEventsRuning(context))
 		{
-			// must by false for avoiding starts/pause events before restart events
+			// must by false for avoiding starts/pause events from receivers before restart events
 			GlobalData.setApplicationStarted(context, false);
 			dataWrapper.firstStartEvents(true);
 		}
