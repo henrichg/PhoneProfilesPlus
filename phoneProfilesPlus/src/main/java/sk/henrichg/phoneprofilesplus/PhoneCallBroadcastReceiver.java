c03a89f72194d6d9ddfe2fee8b08e9747200f0f7
henrichg <henrich.gron@chello.sk>
2015-07-09 17:50:20 +0200

    Fixed muted volume after first call when activated profile has enabled speaker phone

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneCallBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneCallBroadcastReceiver.java
@@ -10,87 +10,89 @@ import java.util.Date;
 
 public class PhoneCallBroadcastReceiver extends PhoneCallReceiver {
 
-	private static AudioManager audioManager = null;
-	
-	private static boolean savedSpeakerphone = false;
-	private static boolean speakerphoneSelected = false;
+    private static AudioManager audioManager = null;
 
-	public static final String BROADCAST_RECEIVER_TYPE = "phoneCall";
-	
-	public static final int CALL_EVENT_UNDEFINED = 0; 
-	public static final int CALL_EVENT_INCOMING_CALL_RINGING = 1; 
-	public static final int CALL_EVENT_OUTGOING_CALL_STARTED = 2;
-	public static final int CALL_EVENT_INCOMING_CALL_ANSWERED = 3; 
-	public static final int CALL_EVENT_OUTGOING_CALL_ANSWERED = 4;
-	public static final int CALL_EVENT_INCOMING_CALL_ENDED = 5; 
-	public static final int CALL_EVENT_OUTGOING_CALL_ENDED = 6;
-	
-	protected boolean onStartReceive()
-	{
-		if (!GlobalData.getApplicationStarted(savedContext))
-			return false;
-		
-		GlobalData.loadPreferences(savedContext);
-		
-		return true;
-	}
+    private static boolean savedSpeakerphone = false;
+    private static boolean speakerphoneSelected = false;
 
-	protected void onEndReceive()
-	{
-	}
-	
-	private void doCallEvent(int eventType, String phoneNumber, DataWrapper dataWrapper)
-	{
-		SharedPreferences preferences = savedContext.getSharedPreferences(GlobalData.APPLICATION_PREFS_NAME, Context.MODE_PRIVATE);
-		Editor editor = preferences.edit();
-		editor.putInt(GlobalData.PREF_EVENT_CALL_EVENT_TYPE, eventType);
-		editor.putString(GlobalData.PREF_EVENT_CALL_PHONE_NUMBER, phoneNumber);
-		editor.commit();
+    public static final String BROADCAST_RECEIVER_TYPE = "phoneCall";
+
+    public static final int CALL_EVENT_UNDEFINED = 0;
+    public static final int CALL_EVENT_INCOMING_CALL_RINGING = 1;
+    public static final int CALL_EVENT_OUTGOING_CALL_STARTED = 2;
+    public static final int CALL_EVENT_INCOMING_CALL_ANSWERED = 3;
+    public static final int CALL_EVENT_OUTGOING_CALL_ANSWERED = 4;
+    public static final int CALL_EVENT_INCOMING_CALL_ENDED = 5;
+    public static final int CALL_EVENT_OUTGOING_CALL_ENDED = 6;
+
+    protected boolean onStartReceive()
+    {
+        if (!GlobalData.getApplicationStarted(savedContext))
+            return false;
+
+        GlobalData.loadPreferences(savedContext);
+
+        return true;
+    }
+
+    protected void onEndReceive()
+    {
+    }
+
+    private void doCallEvent(int eventType, String phoneNumber, DataWrapper dataWrapper)
+    {
+        SharedPreferences preferences = savedContext.getSharedPreferences(GlobalData.APPLICATION_PREFS_NAME, Context.MODE_PRIVATE);
+        Editor editor = preferences.edit();
+        editor.putInt(GlobalData.PREF_EVENT_CALL_EVENT_TYPE, eventType);
+        editor.putString(GlobalData.PREF_EVENT_CALL_PHONE_NUMBER, phoneNumber);
+        editor.commit();
 
         boolean callEventsExists = false;
 
         if (GlobalData.getGlobalEventsRuning(savedContext))
-		{
-			callEventsExists = dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_CALL) > 0;
-			
-			if (callEventsExists)
-			{
-				// start service
-				Intent eventsServiceIntent = new Intent(savedContext, EventsService.class);
-				eventsServiceIntent.putExtra(GlobalData.EXTRA_BROADCAST_RECEIVER_TYPE, BROADCAST_RECEIVER_TYPE);
-				startWakefulService(savedContext, eventsServiceIntent);
-			}
-		}
+        {
+            callEventsExists = dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_CALL) > 0;
 
-		if (GlobalData.applicationUnlinkRingerNotificationVolumes) {
-			if (((eventType == CALL_EVENT_INCOMING_CALL_RINGING) || (eventType == CALL_EVENT_INCOMING_CALL_ENDED))
-					&& (!callEventsExists)) {
-				/// for linked ringer and notification volume:
-				//    notification volume in profile activation is set after ringer volume
-				//    therefore reset ringer volume
-				Profile profile = dataWrapper.getActivatedProfile();
-				if (profile != null) {
-					Intent volumeServiceIntent = new Intent(savedContext, ExecuteVolumeProfilePrefsService.class);
-					volumeServiceIntent.putExtra(GlobalData.EXTRA_PROFILE_ID, profile._id);
-					savedContext.startService(volumeServiceIntent);
-				}
-				///
-			}
-		}
+            if (callEventsExists)
+            {
+                // start service
+                Intent eventsServiceIntent = new Intent(savedContext, EventsService.class);
+                eventsServiceIntent.putExtra(GlobalData.EXTRA_BROADCAST_RECEIVER_TYPE, BROADCAST_RECEIVER_TYPE);
+                startWakefulService(savedContext, eventsServiceIntent);
+            }
+        }
+
+        if (GlobalData.applicationUnlinkRingerNotificationVolumes) {
+            if (((eventType == CALL_EVENT_INCOMING_CALL_RINGING) || (eventType == CALL_EVENT_INCOMING_CALL_ENDED))
+                    && (!callEventsExists)) {
+                /// for linked ringer and notification volume:
+                //    notification volume in profile activation is set after ringer volume
+                //    therefore reset ringer volume
+                Profile profile = dataWrapper.getActivatedProfile();
+                if (profile != null) {
+                    Intent volumeServiceIntent = new Intent(savedContext, ExecuteVolumeProfilePrefsService.class);
+                    volumeServiceIntent.putExtra(GlobalData.EXTRA_PROFILE_ID, profile._id);
+                    savedContext.startService(volumeServiceIntent);
+                }
+                ///
+            }
+        }
 
     }
-	
-	private void callStarted(boolean incoming, String phoneNumber)
-	{
-		if (audioManager == null )
-			audioManager = (AudioManager)savedContext.getSystemService(Context.AUDIO_SERVICE);
+
+    private void callStarted(boolean incoming, String phoneNumber)
+    {
+        if (audioManager == null )
+            audioManager = (AudioManager)savedContext.getSystemService(Context.AUDIO_SERVICE);
+
+        speakerphoneSelected = false;
 
         if (incoming) {
             DataWrapper dataWrapper = new DataWrapper(savedContext, false, false, 0);
 
             doCallEvent(CALL_EVENT_INCOMING_CALL_RINGING, phoneNumber, dataWrapper);
 
-			dataWrapper.invalidateDataWrapper();
+            dataWrapper.invalidateDataWrapper();
         }
         /*else {
             DataWrapper dataWrapper = new DataWrapper(savedContext, false, false, 0);
@@ -100,16 +102,16 @@ public class PhoneCallBroadcastReceiver extends PhoneCallReceiver {
 
             dataWrapper.invalidateDataWrapper();
         }*/
-	}
-	
-	private void callAnswered(boolean incoming, String phoneNumber)
-	{
-		DataWrapper dataWrapper = new DataWrapper(savedContext, false, false, 0);
+    }
 
-		if (incoming)
-			doCallEvent(CALL_EVENT_INCOMING_CALL_ANSWERED, phoneNumber, dataWrapper);
-		else
-			doCallEvent(CALL_EVENT_OUTGOING_CALL_ANSWERED, phoneNumber, dataWrapper);
+    private void callAnswered(boolean incoming, String phoneNumber)
+    {
+        DataWrapper dataWrapper = new DataWrapper(savedContext, false, false, 0);
+
+        if (incoming)
+            doCallEvent(CALL_EVENT_INCOMING_CALL_ANSWERED, phoneNumber, dataWrapper);
+        else
+            doCallEvent(CALL_EVENT_OUTGOING_CALL_ANSWERED, phoneNumber, dataWrapper);
 
         Profile profile = dataWrapper.getActivatedProfile();
         profile = GlobalData.getMappedProfile(profile, savedContext);
@@ -130,81 +132,81 @@ public class PhoneCallBroadcastReceiver extends PhoneCallReceiver {
                 audioManager.setMode(AudioManager.MODE_IN_CALL);
 
                 savedSpeakerphone = audioManager.isSpeakerphoneOn();
-				boolean changeSpeakerphone = false;
-				if (savedSpeakerphone && (profile._volumeSpeakerPhone == 2))
-					changeSpeakerphone = true;
-				if ((!savedSpeakerphone) && (profile._volumeSpeakerPhone == 1))
-					changeSpeakerphone = true;
-				if (changeSpeakerphone) {
+                boolean changeSpeakerphone = false;
+                if (savedSpeakerphone && (profile._volumeSpeakerPhone == 2))
+                    changeSpeakerphone = true;
+                if ((!savedSpeakerphone) && (profile._volumeSpeakerPhone == 1))
+                    changeSpeakerphone = true;
+                if (changeSpeakerphone) {
                     audioManager.setSpeakerphoneOn(profile._volumeSpeakerPhone == 1);
                     speakerphoneSelected = true;
                 }
 
             }
         }
-		
-		dataWrapper.invalidateDataWrapper();
-	}
 
-	private void callEnded(boolean incoming, String phoneNumber)
-	{
-    	//Deactivate loudspeaker
-		if (audioManager == null )
-			audioManager = (AudioManager)savedContext.getSystemService(Context.AUDIO_SERVICE);
+        dataWrapper.invalidateDataWrapper();
+    }
+
+    private void callEnded(boolean incoming, String phoneNumber)
+    {
+        //Deactivate loudspeaker
+        if (audioManager == null )
+            audioManager = (AudioManager)savedContext.getSystemService(Context.AUDIO_SERVICE);
 
         //if (audioManager.isSpeakerphoneOn())
-       	if (speakerphoneSelected)
-    	{
-    	    audioManager.setSpeakerphoneOn(savedSpeakerphone);
-    		speakerphoneSelected = false;
-			audioManager.setMode(AudioManager.MODE_NORMAL);
+        if (speakerphoneSelected)
+        {
+            audioManager.setSpeakerphoneOn(savedSpeakerphone);
+            audioManager.setMode(AudioManager.MODE_NORMAL);
         }
 
+        speakerphoneSelected = false;
 
         DataWrapper dataWrapper = new DataWrapper(savedContext, false, false, 0);
 
 
-		if (incoming)
+        if (incoming)
             doCallEvent(CALL_EVENT_INCOMING_CALL_ENDED, phoneNumber, dataWrapper);
-		else
-			doCallEvent(CALL_EVENT_OUTGOING_CALL_ENDED, phoneNumber, dataWrapper);
+        else
+            doCallEvent(CALL_EVENT_OUTGOING_CALL_ENDED, phoneNumber, dataWrapper);
 
-		dataWrapper.invalidateDataWrapper();
-	}
-	
+        dataWrapper.invalidateDataWrapper();
+    }
+
     protected void onIncomingCallStarted(String number, Date start) 
     {
-    	callStarted(true, number);
+        callStarted(true, number);
     }
 
     protected void onOutgoingCallStarted(String number, Date start)
     {
-    	callStarted(false, number);
+        callStarted(false, number);
     }
     
     protected void onIncomingCallAnswered(String number, Date start)
     {
-    	callAnswered(true, number);
+        callAnswered(true, number);
     }
 
-	protected void onOutgoingCallAnswered(String number, Date start)
-	{
-    	callAnswered(false, number);
-	}
+    protected void onOutgoingCallAnswered(String number, Date start)
+    {
+        callAnswered(false, number);
+    }
     
     protected void onIncomingCallEnded(String number, Date start, Date end)
     {
-    	callEnded(true, number);
+        callEnded(true, number);
     }
 
     protected void onOutgoingCallEnded(String number, Date start, Date end)
     {
-    	callEnded(false, number);
+        callEnded(false, number);
     }
 
     protected void onMissedCall(String number, Date start)
     {
-    	callEnded(true, number);
+        callEnded(true, number);
     }
 
 }
