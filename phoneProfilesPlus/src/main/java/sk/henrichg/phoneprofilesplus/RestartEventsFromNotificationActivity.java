61198791d31bc3b613023b4ea1824739c452622d
henrichg <henrich.gron@chello.sk>
2015-07-15 00:45:13 +0200

    Fixed FC

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/RestartEventsFromNotificationActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/RestartEventsFromNotificationActivity.java
@@ -5,44 +5,42 @@ import android.os.Bundle;
 
 public class RestartEventsFromNotificationActivity extends Activity
 {
-	
-	private DataWrapper dataWrapper;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
 
-		GlobalData.loadPreferences(getApplicationContext());
-		
-		dataWrapper = new DataWrapper(getApplicationContext(), false, false, 0);
-	 
-	}
+    private DataWrapper dataWrapper;
 
-	@Override
-	protected void onStart()
-	{
-		super.onStart();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// set theme and language for dialog alert ;-)
-		// not working on Android 2.3.x
-		GUIData.setTheme(this, true, false);
-		GUIData.setLanguage(getBaseContext());
+        GlobalData.loadPreferences(getApplicationContext());
+
+        dataWrapper = new DataWrapper(getApplicationContext(), false, false, 0);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        // set theme and language for dialog alert ;-)
+        // not working on Android 2.3.x
+        GUIData.setTheme(this, true, false);
+        GUIData.setLanguage(getBaseContext());
 
         dataWrapper.getDatabaseHandler().addActivityLog(DatabaseHandler.ALTYPE_RESTARTEVENTS, null, null, null, 0);
 
-		GlobalData.logE("$$$ restartEvents", "from RestartEventsFromNotificationActivity.onStart");
-		dataWrapper.restartEventsWithAlert(this);
+        GlobalData.logE("$$$ restartEvents", "from RestartEventsFromNotificationActivity.onStart");
+        dataWrapper.restartEventsWithAlert(this);
+    }
 
-        finish();
-	}
-	
-	@Override
-	protected void onDestroy()
-	{
-		super.onDestroy();
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
 
-		dataWrapper.invalidateDataWrapper();
-		dataWrapper = null;
-	}
+        dataWrapper.invalidateDataWrapper();
+        dataWrapper = null;
+    }
 
 }
