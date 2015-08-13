20ac72e28e2bf794e5b3a4ed4c112fc29e4ffaf5
henrichg <henrich.gron@chello.sk>
2015-08-13 03:30:55 +0200

    Changed SMS event type - working as Notification type

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GlobalData.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GlobalData.java
@@ -84,6 +84,7 @@ public class GlobalData extends Application {
                                          //"$$$ setEventsBlocked|"+
                                          //"$$$ restartEvents"
                                          //"$$$ WifiAP"
+            ""
             ;
 
 
