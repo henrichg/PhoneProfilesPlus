456ff8f211ab76f0be5aef87d6320659fd3a2899
henrichg <henrich.gron@chello.sk>
2015-09-14 15:08:11 +0200

    New PPHelper version

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesHelper.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesHelper.java
@@ -37,7 +37,7 @@ public class PhoneProfilesHelper {
 
     public static int PPHelperVersion = -1;
 
-    public static final int PPHELPER_CURRENT_VERSION = 51;
+    public static final int PPHELPER_CURRENT_VERSION = 52;
 
     private static boolean errorNoRoot = false;
     private static boolean nowPPHelperUninstalled = false;
