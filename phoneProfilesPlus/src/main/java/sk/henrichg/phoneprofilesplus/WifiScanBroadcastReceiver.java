b748ceaf8b7a32b45068b95d8fed7aecfcefb59a
henrichg <henrich.gron@chello.sk>
2015-10-01 23:54:53 +0200

    Fixed wifi scanning when application is in background (Android M)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/WifiScanBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/WifiScanBroadcastReceiver.java
@@ -15,7 +15,7 @@ public class WifiScanBroadcastReceiver extends WakefulBroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         //GlobalData.logE("#### WifiScanBroadcastReceiver.onReceive","xxx");
-        GlobalData.logE("@@@ WifiScanBroadcastReceiver.onReceive","----- start");
+        GlobalData.logE("@@@ WifiScanBroadcastReceiver.onReceive", "----- start");
 
         if (WifiScanAlarmBroadcastReceiver.wifi == null)
             WifiScanAlarmBroadcastReceiver.wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -36,43 +36,48 @@ public class WifiScanBroadcastReceiver extends WakefulBroadcastReceiver {
 
             if (scanStarted)
             {
-                GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive","xxx");
+                GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive", "xxx");
 
-                WifiScanAlarmBroadcastReceiver.fillWifiConfigurationList(context);
-                WifiScanAlarmBroadcastReceiver.fillScanResults(context);
-                //WifiScanAlarmBroadcastReceiver.unlock();
+                if ((android.os.Build.VERSION.SDK_INT < 23) || (intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false))) {
 
-                List<WifiSSIDData> scanResults = WifiScanAlarmBroadcastReceiver.getScanResults(context);
-                if (scanResults != null)
-                {
-                    for (WifiSSIDData result : scanResults)
+                    GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive", "resultsUpdated=true");
+
+                    WifiScanAlarmBroadcastReceiver.fillWifiConfigurationList(context);
+                    WifiScanAlarmBroadcastReceiver.fillScanResults(context);
+                    //WifiScanAlarmBroadcastReceiver.unlock();
+
+                    List<WifiSSIDData> scanResults = WifiScanAlarmBroadcastReceiver.getScanResults(context);
+                    if (scanResults != null) {
+                        for (WifiSSIDData result : scanResults) {
+                            GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive", "result.SSID=" + result.ssid);
+                        }
+                    }
+
+                    /*
+                    if (WifiScanAlarmBroadcastReceiver.getWifiEnabledForScan(context))
                     {
-                        GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive","result.SSID="+result.ssid);
+                        GlobalData.logE("@@@ WifiScanBroadcastReceiver.onReceive","disable wifi");
+                        WifiScanAlarmBroadcastReceiver.wifi.setWifiEnabled(false);
+                        // not call this, due WifiConnectionBroadcastReceiver
+                        //WifiScanAlarmBroadcastReceiver.setWifiEnabledForScan(context, false);
+                    }
+                    */
+
+                    WifiScanAlarmBroadcastReceiver.setWaitForResults(context, false);
+
+                    boolean forceOneScan = GlobalData.getForceOneWifiScan(context);
+                    GlobalData.setForceOneWifiScan(context, false);
+
+                    if (!forceOneScan) // not start service for force scan
+                    {
+                        // start service
+                        Intent eventsServiceIntent = new Intent(context, EventsService.class);
+                        eventsServiceIntent.putExtra(GlobalData.EXTRA_BROADCAST_RECEIVER_TYPE, BROADCAST_RECEIVER_TYPE);
+                        startWakefulService(context, eventsServiceIntent);
                     }
                 }
-
-                /*
-                if (WifiScanAlarmBroadcastReceiver.getWifiEnabledForScan(context))
-                {
-                    GlobalData.logE("@@@ WifiScanBroadcastReceiver.onReceive","disable wifi");
-                    WifiScanAlarmBroadcastReceiver.wifi.setWifiEnabled(false);
-                    // not call this, due WifiConnectionBroadcastReceiver
-                    //WifiScanAlarmBroadcastReceiver.setWifiEnabledForScan(context, false);
-                }
-                */
-
-                WifiScanAlarmBroadcastReceiver.setWaitForResults(context, false);
-
-                boolean forceOneScan = GlobalData.getForceOneWifiScan(context);
-                GlobalData.setForceOneWifiScan(context, false);
-
-                if (!forceOneScan) // not start service for force scan
-                {
-                    // start service
-                    Intent eventsServiceIntent = new Intent(context, EventsService.class);
-                    eventsServiceIntent.putExtra(GlobalData.EXTRA_BROADCAST_RECEIVER_TYPE, BROADCAST_RECEIVER_TYPE);
-                    startWakefulService(context, eventsServiceIntent);
-                }
+                else
+                    GlobalData.logE("$$$ WifiScanBroadcastReceiver.onReceive", "resultsUpdated=false");
 
             }
 
