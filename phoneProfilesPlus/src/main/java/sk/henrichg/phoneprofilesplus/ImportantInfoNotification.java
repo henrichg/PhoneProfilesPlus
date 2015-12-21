60d5406d310ef2110983ccbb8b713d627919a430
henrichg <henrich.gron@chello.sk>
2015-12-21 17:35:11 +0100

    New version 3.3.1.1

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
@@ -12,7 +12,7 @@ import android.support.v4.app.NotificationCompat;
 public class ImportantInfoNotification {
 
     // this version code must by <= version code in manifest
-    public static final int VERSION_CODE_FOR_NEWS = 1832;
+    public static final int VERSION_CODE_FOR_NEWS = 1834;
 
     static public void showInfoNotification(Context context) {
 
