967c66986081044af8ffff06cad1f6e59fc44248
henrichg <henrich.gron@chello.sk>
2016-03-29 13:15:18 +0200

    Fixed bug

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileDurationAlarmBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileDurationAlarmBroadcastReceiver.java
@@ -83,7 +83,7 @@ public class ProfileDurationAlarmBroadcastReceiver extends BroadcastReceiver {
     @SuppressLint({"SimpleDateFormat", "NewApi"})
     static public void setAlarm(Profile profile, Context context)
     {
-        removeAlarm(profile, context);
+        removeAlarm(context);
 
         if (profile == null)
             return;
@@ -104,7 +104,7 @@ public class ProfileDurationAlarmBroadcastReceiver extends BroadcastReceiver {
             Intent intent = new Intent(context, ProfileDurationAlarmBroadcastReceiver.class);
             intent.putExtra(GlobalData.EXTRA_PROFILE_ID, profile._id);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), (int) profile._id, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Activity.ALARM_SERVICE);
 
@@ -128,13 +128,13 @@ public class ProfileDurationAlarmBroadcastReceiver extends BroadcastReceiver {
         return;
     }
 
-    static public void removeAlarm(Profile profile, Context context)
+    static public void removeAlarm(Context context)
     {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Activity.ALARM_SERVICE);
 
         Intent intent = new Intent(context, ProfileDurationAlarmBroadcastReceiver.class);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), (int) profile._id, intent, PendingIntent.FLAG_NO_CREATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), 0, intent, PendingIntent.FLAG_NO_CREATE);
         if (pendingIntent != null)
         {
             alarmManager.cancel(pendingIntent);
