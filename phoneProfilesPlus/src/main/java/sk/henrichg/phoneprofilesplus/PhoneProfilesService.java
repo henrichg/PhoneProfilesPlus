3d1fa8fc3ae4501fe1c6e5201b943cd8d6420642
henrichg <henrich.gron@chello.sk>
2016-05-27 17:03:51 +0200

    Fix for simulating ringing - 2

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesService.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesService.java
@@ -577,9 +577,9 @@ public class PhoneProfilesService extends Service
                 usedStream = stream;
                 // play repeating: default ringtone with ringing volume level
                 try {
-                    int requestType = AudioManager.AUDIOFOCUS_GAIN;
+                    int requestType = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT;
                     if (android.os.Build.VERSION.SDK_INT >= 19)
-                        requestType = AudioManager.AUDIOFOCUS_GAIN;
+                        requestType = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE;
                     int result = audioManager.requestAudioFocus(this, usedStream, requestType);
                     if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
                         ringingMediaPlayer = new MediaPlayer();
