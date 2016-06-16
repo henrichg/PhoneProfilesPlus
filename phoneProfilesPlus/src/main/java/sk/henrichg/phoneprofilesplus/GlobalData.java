320b6410329e1438a7412a01b7a997c055dc7a18
henrichg <henrich.gron@chello.sk>
2016-06-16 13:56:10 +0200

    Removed logs

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GlobalData.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GlobalData.java
@@ -49,8 +49,8 @@ public class GlobalData extends Application {
 
     public static final boolean exactAlarms = true;
 
-    private static boolean logIntoLogCat = true;
-    private static boolean logIntoFile = true;
+    private static boolean logIntoLogCat = false;
+    private static boolean logIntoFile = false;
     private static boolean rootToolsDebug = false;
     public static String logFilterTags =  "PhoneProfilesHelper.doUninstallPPHelper"
 
