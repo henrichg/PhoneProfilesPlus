3debd82200c5c2dc4ddf101b613d56e6f84bc8d5
henrichg <henrich.gron@chello.sk>
2016-12-17 15:13:06 +0100

    Fixed FC: java.lang.IllegalStateException: Cannot start this animator on a detached view! (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GuiInfoPopupWindow.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GuiInfoPopupWindow.java
@@ -39,33 +39,34 @@ class GuiInfoPopupWindow extends RelativePopupWindow {
     public void showOnAnchor(@NonNull View anchor, int vertPos, int horizPos, int x, int y) {
         super.showOnAnchor(anchor, vertPos, horizPos, x, y);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (ViewCompat.isAttachedToWindow(view))
-                circularReveal(anchor);
+            circularReveal(anchor);
         }
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void circularReveal(@NonNull final View anchor) {
         final View contentView = getContentView();
-        contentView.post(new Runnable() {
-            @Override
-            public void run() {
-                final int[] myLocation = new int[2];
-                final int[] anchorLocation = new int[2];
-                contentView.getLocationOnScreen(myLocation);
-                anchor.getLocationOnScreen(anchorLocation);
-                final int cx = anchorLocation[0] - myLocation[0] + anchor.getWidth()/2;
-                final int cy = anchorLocation[1] - myLocation[1] + anchor.getHeight()/2;
+        if (ViewCompat.isAttachedToWindow(anchor)) {
+            contentView.post(new Runnable() {
+                @Override
+                public void run() {
+                    final int[] myLocation = new int[2];
+                    final int[] anchorLocation = new int[2];
+                    contentView.getLocationOnScreen(myLocation);
+                    anchor.getLocationOnScreen(anchorLocation);
+                    final int cx = anchorLocation[0] - myLocation[0] + anchor.getWidth() / 2;
+                    final int cy = anchorLocation[1] - myLocation[1] + anchor.getHeight() / 2;
 
-                contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-                final int dx = Math.max(cx, contentView.getMeasuredWidth() - cx);
-                final int dy = Math.max(cy, contentView.getMeasuredHeight() - cy);
-                final float finalRadius = (float) Math.hypot(dx, dy);
-                Animator animator = ViewAnimationUtils.createCircularReveal(contentView, cx, cy, 0f, finalRadius);
-                animator.setDuration(500);
-                animator.start();
-            }
-        });
+                    contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+                    final int dx = Math.max(cx, contentView.getMeasuredWidth() - cx);
+                    final int dy = Math.max(cy, contentView.getMeasuredHeight() - cy);
+                    final float finalRadius = (float) Math.hypot(dx, dy);
+                    Animator animator = ViewAnimationUtils.createCircularReveal(contentView, cx, cy, 0f, finalRadius);
+                    animator.setDuration(500);
+                    animator.start();
+                }
+            });
+        }
     }
 
 }
