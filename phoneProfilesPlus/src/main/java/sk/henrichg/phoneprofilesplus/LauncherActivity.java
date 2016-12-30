c21fe5cc086f4f2f4919e7279a9eb10390064a90
henrichg <henrich.gron@chello.sk>
2016-12-30 09:36:09 +0100

    Calling finish() in LauncherActivity after start Activator/Editor.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/LauncherActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/LauncherActivity.java
@@ -98,14 +98,14 @@ public class LauncherActivity extends Activity {
                 break;
         }
 
-        finish();
-
         intentLaunch.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intentLaunch.putExtra(GlobalData.EXTRA_STARTUP_SOURCE, startupSource);
         getApplicationContext().startActivity(intentLaunch);
 
         // reset, aby sa to dalej chovalo ako normalne spustenie z lauchera
         startupSource = 0;
+
+        finish();
     }
 
     @Override
