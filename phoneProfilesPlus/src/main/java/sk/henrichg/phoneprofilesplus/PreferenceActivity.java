572d0cd7524a5aa9ea3e126df4a80cc78451a9a8
henrichg <henrich.gron@chello.sk>
2017-02-01 19:34:21 +0100

    Moved materialPreferences to phoneProfilesPlus. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PreferenceActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PreferenceActivity.java
@@ -38,7 +38,7 @@ public abstract class PreferenceActivity extends AppCompatPreferenceActivity
         // Do not add custom layout for lollipop devices or we lose the widgets animation
         // (app compat bug?)
         //if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            setContentView(R.layout.mp_activity_settings);;
+            setContentView(R.layout.mp_activity_settings);
             Toolbar toolbar = (Toolbar) findViewById(R.id.mp_toolbar);
             setSupportActionBar(toolbar);
         //}
