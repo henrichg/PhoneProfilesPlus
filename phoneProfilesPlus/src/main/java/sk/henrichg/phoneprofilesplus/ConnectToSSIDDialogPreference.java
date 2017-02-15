2a4fc539c461ee423467f0789e5de6de1892d1e9
henrichg <henrich.gron@chello.sk>
2017-02-15 16:48:19 +0100

    Added "Connect to SSID" profile preference. (1)

---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ConnectToSSIDDialogPreference.java
@@ -0,0 +1,220 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.mobeta.android.dslv.DragSortListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ConnectToSSIDDialogPreference extends DialogPreference {
+
+    Context context;
+
+    String value = "";
+
+    List<WifiSSIDData> ssidList = null;
+
+    private MaterialDialog mDialog;
+
+    private ListView listView;
+    private LinearLayout linlaProgress;
+
+    private ConnectToSSIDPreferenceAdapter listAdapter;
+
+    public ConnectToSSIDDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        ssidList = new ArrayList<>();
+
+        //setWidgetLayoutResource(R.layout.applications_preference); // resource na layout custom preference - TextView-ImageView
+
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+
+        MaterialDialog.Builder mBuilder = new MaterialDialog.Builder(getContext())
+                .title(getDialogTitle())
+                .icon(getDialogIcon())
+                //.disableDefaultFonts()
+                .autoDismiss(false)
+                .content(getDialogMessage());
+
+        mBuilder.positiveText(getPositiveButtonText())
+                .negativeText(getNegativeButtonText());
+        mBuilder.onPositive(new MaterialDialog.SingleButtonCallback() {
+            @Override
+            public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
+                if (callChangeListener(value))
+                {
+                    persistString(value);
+                }
+                mDialog.dismiss();
+            }
+        });
+        mBuilder.onNegative(new MaterialDialog.SingleButtonCallback() {
+            @Override
+            public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
+                mDialog.dismiss();
+            }
+        });
+
+        View layout = LayoutInflater.from(getContext()).inflate(R.layout.activity_connect_to_ssid_pref_dialog, null);
+        onBindDialogView(layout);
+
+        listView = (ListView) layout.findViewById(R.id.connect_to_ssid_pref_dlg_listview);
+        linlaProgress = (LinearLayout)layout.findViewById(R.id.connect_to_ssid_pref_dlg_linla_progress);
+
+        listAdapter = new ConnectToSSIDPreferenceAdapter(context, this);
+
+        mBuilder.customView(layout, false);
+
+        mBuilder.showListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                ConnectToSSIDDialogPreference.this.onShow(dialog);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                value = ssidList.get(position).ssid;
+            }
+        });
+
+        mDialog = mBuilder.build();
+        if (state != null)
+            mDialog.onRestoreInstanceState(state);
+
+        mDialog.setOnDismissListener(this);
+        mDialog.show();
+    }
+
+    public void onShow(DialogInterface dialog) {
+
+        new AsyncTask<Void, Integer, Void>() {
+
+            List<WifiSSIDData> _SSIDList = null;
+
+            @Override
+            protected void onPreExecute()
+            {
+                super.onPreExecute();
+
+                _SSIDList = new ArrayList<>();
+
+                listView.setVisibility(View.GONE);
+                linlaProgress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                _SSIDList.add(new WifiSSIDData("-", "", false));
+
+                List<WifiSSIDData> wifiConfigurationList = WifiScanAlarmBroadcastReceiver.getWifiConfigurationList(context);
+                if (wifiConfigurationList != null)
+                {
+                    for (WifiSSIDData wifiConfiguration : wifiConfigurationList)
+                    {
+                        if (wifiConfiguration.ssid != null)
+                            _SSIDList.add(new WifiSSIDData(wifiConfiguration.ssid.replace("\"", ""), wifiConfiguration.bssid, false));
+                    }
+                }
+
+                Collections.sort(_SSIDList, new ConnectToSSIDDialogPreference.SortList());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result)
+            {
+                super.onPostExecute(result);
+
+                ssidList = new ArrayList<>(_SSIDList);
+                listAdapter.notifyDataSetChanged();
+
+                linlaProgress.setVisibility(View.GONE);
+                listView.setVisibility(View.VISIBLE);
+            }
+
+        }.execute();
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray ta, int index)
+    {
+        super.onGetDefaultValue(ta, index);
+        return ta.getString(index);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue)
+    {
+        if (restoreValue) {
+            // restore state
+            getValueCTSDP();
+        }
+        else {
+            // set state
+            // sem narvi default string aplikacii oddeleny |
+            value = "";
+            persistString("");
+        }
+        setSummaryCTSDP();
+    }
+
+    private void getValueCTSDP()
+    {
+        // Get the persistent value
+        value = getPersistedString(value);
+        //Log.d("ApplicationsDialogPreference.getValueAMSDP","value="+value);
+    }
+
+    private void setSummaryCTSDP()
+    {
+        String prefSummary = context.getString(R.string.connect_to_ssid_pref_dlg_summary_text_just_any);
+        if (!value.isEmpty() && !value.equals("-")) {
+            prefSummary = value;
+        }
+        setSummary(prefSummary);
+    }
+
+    private class SortList implements Comparator<WifiSSIDData> {
+
+        public int compare(WifiSSIDData lhs, WifiSSIDData rhs) {
+            return GlobalGUIRoutines.collator.compare(lhs.ssid, rhs.ssid);
+        }
+
+    }
+
+}
