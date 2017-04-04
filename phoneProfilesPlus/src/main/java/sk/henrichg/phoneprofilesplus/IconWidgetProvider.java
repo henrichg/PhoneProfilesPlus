9ddb1b2a7b1d3829d737de02cd14621ea6f7b797
henrichg <henrich.gron@chello.sk>
2017-04-04 15:55:38 +0200

    Added showing activation duration to icon widget.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/IconWidgetProvider.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/IconWidgetProvider.java
@@ -45,7 +45,7 @@ public class IconWidgetProvider extends AppWidgetProvider {
             {
                 isIconResourceID = profile.getIsIconResourceID();
                 iconIdentifier = profile.getIconIdentifier();
-                profileName = dataWrapper.getProfileNameWithManualIndicator(profile, false, false, false);
+                profileName = dataWrapper.getProfileNameWithManualIndicator(profile, false, true, true);
             }
             else
             {
