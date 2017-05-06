f74be826e6e8a8ea17829b077450780b3ced47a4
henrichg <henrich.gron@chello.sk>
2017-05-06 10:35:52 +0200

    Fixed FC: Caused by java.lang.NullPointerException: Attempt to read from field 'long sk.henrichg.phoneprofilesplus.Profile._id' on a null object reference                  at sk.henrichg.phoneprofilesplus.ProfileDurationAlarmBroadcastReceiver.onReceive(ProfileDurationAlarmBroadcastReceiver.java:34)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileDurationAlarmBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileDurationAlarmBroadcastReceiver.java
@@ -31,7 +31,7 @@ public class ProfileDurationAlarmBroadcastReceiver extends BroadcastReceiver {
                     Profile profile = dataWrapper.getProfileById(profileId, false);
                     Profile activatedProfile = dataWrapper.getActivatedProfile();
 
-                    if ((profile != null) &&
+                    if ((profile != null) && (activatedProfile != null) &&
                         (activatedProfile._id == profile._id) &&
                         (profile._afterDurationDo != Profile.AFTERDURATIONDO_NOTHING))
                     {
