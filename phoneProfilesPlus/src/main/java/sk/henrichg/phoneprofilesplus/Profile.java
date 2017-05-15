ce87b9c0a1f59bbe7d20c918830622fb59b23158
henrichg <henrich.gron@chello.sk>
2017-05-15 15:41:04 +0200

    Fixed FC: Fatal Exception: java.lang.RuntimeException: Canvas: trying to use a recycled bitmap

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/Profile.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/Profile.java
@@ -1260,7 +1260,8 @@ public class Profile {
     {
         if (_iconBitmap != null)
         {
-            _iconBitmap.recycle();
+            //if (!_iconBitmap.isRecycled())
+            //    _iconBitmap.recycle();
             _iconBitmap = null;
         }
     }
@@ -1269,7 +1270,8 @@ public class Profile {
     {
         if (_preferencesIndicator != null)
         {
-            _preferencesIndicator.recycle();
+            //if (!_preferencesIndicator.isRecycled())
+            //    _preferencesIndicator.recycle();
             _preferencesIndicator = null;
         }
     }
