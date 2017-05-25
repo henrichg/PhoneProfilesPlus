8e6037addc749cdf139762c72c738a091ebc883f
henrichg <henrich.gron@chello.sk>
2017-05-25 11:28:36 +0200

    Fixed FC: Caused by java.lang.NullPointerException: Attempt to invoke virtual method 'int java.lang.String.compareToIgnoreCase(java.lang.String)' on a null object reference                  at com.android.internal.database.SortCursor.<init>(SortCursor.java:95)                  at android.media.RingtoneManager.getCursor(RingtoneManager.java:485)                  at sk.henrichg.phoneprofiles.FirstStartService.getPhoneProfilesSilentUri(FirstStartService.java:111)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/FirstStartService.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/FirstStartService.java
@@ -143,23 +143,25 @@ public class FirstStartService extends IntentService {
     }
 
     static String getPhoneProfilesSilentUri(Context context, int type) {
-        RingtoneManager manager = new RingtoneManager(context);
-        manager.setType(type);
-        Cursor cursor = manager.getCursor();
+        try {
+            RingtoneManager manager = new RingtoneManager(context);
+            manager.setType(type);
+            Cursor cursor = manager.getCursor();
 
-        while (cursor.moveToNext()) {
-            String id = cursor.getString(RingtoneManager.ID_COLUMN_INDEX);
-            String uri = cursor.getString(RingtoneManager.URI_COLUMN_INDEX);
-            String title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX);
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(RingtoneManager.ID_COLUMN_INDEX);
+                String uri = cursor.getString(RingtoneManager.URI_COLUMN_INDEX);
+                String title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX);
 
-            String uriId = uri + "/" + id;
+                String uriId = uri + "/" + id;
 
-            //Log.d("FirstStartService.getPhoneProfilesSilentNotificationUri", "title="+title);
-            //Log.d("FirstStartService.getPhoneProfilesSilentNotificationUri", "uriId="+uriId);
+                //Log.d("FirstStartService.getPhoneProfilesSilentNotificationUri", "title="+title);
+                //Log.d("FirstStartService.getPhoneProfilesSilentNotificationUri", "uriId="+uriId);
 
-            if (title.equals("PhoneProfiles Silent") || title.equals("phoneprofiles_silent"))
-                return uriId;
-        }
+                if (title.equals("PhoneProfiles Silent") || title.equals("phoneprofiles_silent"))
+                    return uriId;
+            }
+        } catch (Exception ignored) {}
         return "";
     }
 
