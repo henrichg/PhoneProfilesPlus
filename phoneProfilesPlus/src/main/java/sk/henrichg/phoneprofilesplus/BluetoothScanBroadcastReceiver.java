a61cff36e7cef7484688c05fab5fda02a97334a5
henrichg <henrich.gron@chello.sk>
2017-06-01 17:57:31 +0200

    Not using exact alarm if not needed.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/BluetoothScanBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/BluetoothScanBroadcastReceiver.java
@@ -193,9 +193,9 @@ public class BluetoothScanBroadcastReceiver extends BroadcastReceiver {
         long alarmTime = calendar.getTimeInMillis();
 
         if (android.os.Build.VERSION.SDK_INT >= 23)
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
+            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
         else if (android.os.Build.VERSION.SDK_INT >= 19)
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
         else
             alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
     }
