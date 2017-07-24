9a39f5a0baa60ee37b4db9447eea810c22a4b71e
henrichg <henrich.gron@chello.sk>
2017-07-24 19:00:46 +0200

    Optimized broadcasts. (6)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/DeviceIdleModeBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/DeviceIdleModeBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.PowerManager;
 
@@ -14,9 +15,6 @@ public class DeviceIdleModeBroadcastReceiver extends BroadcastReceiver {
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        //Thread.setDefaultUncaughtExceptionHandler(new TopExceptionHandler());
-
         PPApplication.logE("##### DeviceIdleModeBroadcastReceiver.onReceive","xxx");
 
         Context appContext = context.getApplicationContext();
@@ -25,42 +23,9 @@ public class DeviceIdleModeBroadcastReceiver extends BroadcastReceiver {
             // application is not started
             return;
 
-        //PPApplication.loadPreferences(appContext);
+        Intent serviceIntent = new Intent(context, DeviceIdleModeService.class);
+        serviceIntent.setAction(intent.getAction());
+        WakefulIntentService.sendWakefulWork(context, serviceIntent);
 
-        if (Event.getGlobalEventsRuning(appContext))
-        {
-            PowerManager powerManager = (PowerManager) appContext.getSystemService(Context.POWER_SERVICE);
-            // isLightDeviceIdleMode() is @hide :-(
-            if (!powerManager.isDeviceIdleMode() /*&& !powerManager.isLightDeviceIdleMode()*/)
-            {
-                // start service
-                Intent eventsServiceIntent = new Intent(appContext, EventsService.class);
-                eventsServiceIntent.putExtra(EventsService.EXTRA_BROADCAST_RECEIVER_TYPE, EventsService.SENSOR_TYPE_DEVICE_IDLE_MODE);
-                WakefulIntentService.sendWakefulWork(appContext, eventsServiceIntent);
-
-                // rescan
-                DataWrapper dataWrapper = new DataWrapper(appContext, false, false, 0);
-                if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_WIFIINFRONT) > 0) {
-                    // schedule job for one wifi scan
-                    WifiScanJob.scheduleJob(context, true, true, false);
-                }
-                if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_BLUETOOTHINFRONT) > 0) {
-                    // schedule job for one bluetooth scan
-                    BluetoothScanJob.scheduleJob(context, true, true);
-                }
-                if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_LOCATION) > 0) {
-                    // schedule job for location scan
-                    PPApplication.logE("GeofenceScannerJob.scheduleJob", "from DeviceIdleModeBroadcastReceiver.onReceive");
-                    GeofenceScannerJob.scheduleJob(context, true, true);
-                }
-                if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_MOBILE_CELLS) > 0) {
-                    // rescan mobile cells
-                    if ((PhoneProfilesService.instance != null) && PhoneProfilesService.isPhoneStateStarted()) {
-                        PhoneProfilesService.phoneStateScanner.rescanMobileCells();
-                    }
-                }
-                dataWrapper.invalidateDataWrapper();
-            }
-        }
     }
 }
---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/DeviceIdleModeService.java
@@ -0,0 +1,65 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.os.Build;
+import android.os.PowerManager;
+
+import com.commonsware.cwac.wakeful.WakefulIntentService;
+
+public class DeviceIdleModeService extends WakefulIntentService {
+
+    public DeviceIdleModeService() {
+        super("DeviceIdleModeService");
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void doWakefulWork(Intent intent) {
+        if (intent != null) {
+
+            Context appContext = getApplicationContext();
+
+            //PPApplication.loadPreferences(appContext);
+
+            if (Event.getGlobalEventsRuning(appContext))
+            {
+                PowerManager powerManager = (PowerManager) appContext.getSystemService(Context.POWER_SERVICE);
+                // isLightDeviceIdleMode() is @hide :-(
+                if (!powerManager.isDeviceIdleMode() /*&& !powerManager.isLightDeviceIdleMode()*/)
+                {
+                    // start service
+                    Intent eventsServiceIntent = new Intent(appContext, EventsService.class);
+                    eventsServiceIntent.putExtra(EventsService.EXTRA_BROADCAST_RECEIVER_TYPE, EventsService.SENSOR_TYPE_DEVICE_IDLE_MODE);
+                    WakefulIntentService.sendWakefulWork(appContext, eventsServiceIntent);
+
+                    // rescan
+                    DataWrapper dataWrapper = new DataWrapper(appContext, false, false, 0);
+                    if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_WIFIINFRONT) > 0) {
+                        // schedule job for one wifi scan
+                        WifiScanJob.scheduleJob(appContext, true, true, false);
+                    }
+                    if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_BLUETOOTHINFRONT) > 0) {
+                        // schedule job for one bluetooth scan
+                        BluetoothScanJob.scheduleJob(appContext, true, true);
+                    }
+                    if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_LOCATION) > 0) {
+                        // schedule job for location scan
+                        PPApplication.logE("GeofenceScannerJob.scheduleJob", "from DeviceIdleModeBroadcastReceiver.onReceive");
+                        GeofenceScannerJob.scheduleJob(appContext, true, true);
+                    }
+                    if (dataWrapper.getDatabaseHandler().getTypeEventsCount(DatabaseHandler.ETYPE_MOBILE_CELLS) > 0) {
+                        // rescan mobile cells
+                        if ((PhoneProfilesService.instance != null) && PhoneProfilesService.isPhoneStateStarted()) {
+                            PhoneProfilesService.phoneStateScanner.rescanMobileCells();
+                        }
+                    }
+                    dataWrapper.invalidateDataWrapper();
+                }
+            }
+        }
+    }
+
+}
