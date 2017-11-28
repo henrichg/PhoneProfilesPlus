9650911af094aaff67f3056cb6e240c91ec3b1e0
henrichg <henrich.gron@chello.sk>
2017-11-29 00:41:58 +0100

    Added repeating notification for start of event. (3)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/StartEventNotificationBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/StartEventNotificationBroadcastReceiver.java
@@ -7,9 +7,13 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.PowerManager;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import static android.content.Context.POWER_SERVICE;
 
 public class StartEventNotificationBroadcastReceiver extends BroadcastReceiver {
 
@@ -18,13 +22,31 @@ public class StartEventNotificationBroadcastReceiver extends BroadcastReceiver {
         PPApplication.logE("##### StartEventNotificationBroadcastReceiver.onReceive", "xxx");
         CallsCounter.logCounter(context, "StartEventNotificationBroadcastReceiver.onReceive", "StartEventNotificationBroadcastReceiver_onReceive");
 
-        long event_id = intent.getLongExtra(PPApplication.EXTRA_EVENT_ID, 0);
-        if (event_id != 0) {
-            DatabaseHandler databaseHandler = DatabaseHandler.getInstance(context.getApplicationContext());
-            Event event = databaseHandler.getEvent(event_id);
-            if (event != null)
-                event.notifyEventStart(context.getApplicationContext());
-        }
+        final Context appContext = context.getApplicationContext();
+        final long event_id = intent.getLongExtra(PPApplication.EXTRA_EVENT_ID, 0);
+        PhoneProfilesService.startHandlerThread();
+        final Handler handler = new Handler(PhoneProfilesService.handlerThread.getLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (event_id != 0) {
+                    PowerManager powerManager = (PowerManager) appContext.getSystemService(POWER_SERVICE);
+                    PowerManager.WakeLock wakeLock = null;
+                    if (powerManager != null) {
+                        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "StartEventNotificationBroadcastReceiver.onReceive");
+                        wakeLock.acquire(10 * 60 * 1000);
+                    }
+
+                    DatabaseHandler databaseHandler = DatabaseHandler.getInstance(appContext);
+                    Event event = databaseHandler.getEvent(event_id);
+                    if (event != null)
+                        event.notifyEventStart(appContext);
+
+                    if ((wakeLock != null) && wakeLock.isHeld())
+                        wakeLock.release();
+                }
+            }
+        });
     }
 
     static void removeAlarm(Context context)
