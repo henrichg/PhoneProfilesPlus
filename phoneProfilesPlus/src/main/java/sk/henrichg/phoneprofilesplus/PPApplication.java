e53cdb3b4170f57198ac67e548ff2da46604ee5e
henrichg <henrich.gron@chello.sk>
2018-01-16 17:56:37 +0100

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -54,7 +54,7 @@ public class PPApplication extends Application {
     static final int VERSION_CODE_EXTENDER = 60;
 
     private static final boolean logIntoLogCat = true;
-    private static final boolean logIntoFile = true;
+    private static final boolean logIntoFile = false;
     private static final boolean rootToolsDebug = false;
     private static final String logFilterTags = "##### PPApplication.onCreate"
                                          +"|PhoneProfilesService.onCreate"
