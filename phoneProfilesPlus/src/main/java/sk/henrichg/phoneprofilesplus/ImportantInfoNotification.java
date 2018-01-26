1c02431b7ec251d3102e7e6ea095be6f221881cb
henrichg <henrich.gron@chello.sk>
2018-01-26 16:25:43 +0100

    New version 3.5.7.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
@@ -12,7 +12,7 @@ import android.support.v4.app.NotificationCompat;
 class ImportantInfoNotification {
 
     // this version code must by <= version code in dependencies.gradle
-    static final int VERSION_CODE_FOR_NEWS = 3670;
+    static final int VERSION_CODE_FOR_NEWS = 3900;
 
     private static final String PREF_SHOW_INFO_NOTIFICATION_ON_START = "show_info_notification_on_start";
     private static final String PREF_SHOW_INFO_NOTIFICATION_ON_START_VERSION = "show_info_notification_on_start_version";
