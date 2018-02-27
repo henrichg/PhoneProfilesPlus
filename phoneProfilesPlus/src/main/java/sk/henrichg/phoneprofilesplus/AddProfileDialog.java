15df6feba9c4d8e3fb8fe4b1d9d46801094c3eef
henrichg <henrich.gron@chello.sk>
2018-02-27 09:46:50 +0100

    Added AsyncTask for generating profile list into "Add profile" dialog.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/AddProfileDialog.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/AddProfileDialog.java
@@ -1,14 +1,19 @@
 package sk.henrichg.phoneprofilesplus;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class AddProfileDialog
@@ -16,26 +21,15 @@ class AddProfileDialog
     private final EditorProfileListFragment profileListFragment;
 
     final MaterialDialog mDialog;
+    final Context context;
+
+    private LinearLayout linlaProgress;
+    private final ListView listView;
 
     AddProfileDialog(Context context, EditorProfileListFragment profileListFragment)
     {
         this.profileListFragment = profileListFragment;
-
-        List<Profile> profileList = new ArrayList<>();
-
-        Profile profile;
-        profile = DataWrapper.getNonInitializedProfile(
-                                        context.getResources().getString(R.string.profile_name_default),
-                                        Profile.PROFILE_ICON_DEFAULT, 0);
-        profile.generateIconBitmap(context, false, 0xFF);
-        profile.generatePreferencesIndicator(context, false, 0xFF);
-        profileList.add(profile);
-        for (int index = 0; index < 6; index++) {
-            profile = profileListFragment.activityDataWrapper.getPredefinedProfile(index, false);
-            profile.generateIconBitmap(context, false, 0xFF);
-            profile.generatePreferencesIndicator(context, false, 0xFF);
-            profileList.add(profile);
-        }
+        this.context = context;
 
         MaterialDialog.Builder dialogBuilder = new MaterialDialog.Builder(context)
                 .title(R.string.new_profile_predefined_profiles_dialog)
@@ -44,13 +38,21 @@ class AddProfileDialog
                 .autoDismiss(true)
                 .customView(R.layout.activity_profile_pref_dialog, false);
 
+        dialogBuilder.showListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                AddProfileDialog.this.onShow(/*dialog*/);
+            }
+        });
+
         mDialog = dialogBuilder.build();
+        View layout = mDialog.getCustomView();
 
         //noinspection ConstantConditions
-        ListView listView = mDialog.getCustomView().findViewById(R.id.profile_pref_dlg_listview);
+        linlaProgress = layout.findViewById(R.id.profile_pref_dlg_linla_progress);
 
-        AddProfileAdapter addProfileAdapter = new AddProfileAdapter(context, profileList);
-        listView.setAdapter(addProfileAdapter);
+        //noinspection ConstantConditions
+        listView = layout.findViewById(R.id.profile_pref_dlg_listview);
 
         listView.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
@@ -59,6 +61,57 @@ class AddProfileDialog
 
         });
 
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void onShow(/*DialogInterface dialog*/) {
+        new AsyncTask<Void, Integer, Void>() {
+
+            List<Profile> profileList = new ArrayList<>();
+
+            @Override
+            protected void onPreExecute()
+            {
+                super.onPreExecute();
+                listView.setVisibility(View.GONE);
+                linlaProgress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                Profile profile;
+                profile = DataWrapper.getNonInitializedProfile(
+                        context.getResources().getString(R.string.profile_name_default),
+                        Profile.PROFILE_ICON_DEFAULT, 0);
+                profile.generateIconBitmap(context, false, 0xFF);
+                if (ApplicationPreferences.applicationEditorPrefIndicator(context))
+                    profile.generatePreferencesIndicator(context, false, 0xFF);
+                profileList.add(profile);
+                for (int index = 0; index < 6; index++) {
+                    profile = profileListFragment.activityDataWrapper.getPredefinedProfile(index, false);
+                    profile.generateIconBitmap(context, false, 0xFF);
+                    if (ApplicationPreferences.applicationEditorPrefIndicator(context))
+                        profile.generatePreferencesIndicator(context, false, 0xFF);
+                    profileList.add(profile);
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result)
+            {
+                super.onPostExecute(result);
+
+                listView.setVisibility(View.VISIBLE);
+                linlaProgress.setVisibility(View.GONE);
+
+                AddProfileAdapter addProfileAdapter = new AddProfileAdapter(context, profileList);
+                listView.setAdapter(addProfileAdapter);
+            }
+
+        }.execute();
     }
 
     private void doOnItemSelected(int position)
