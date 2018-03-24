4276d52a6048e0b7e1b9f70bca0c321f5c350732
henrichg <henrich.gron@chello.sk>
2018-03-24 12:29:37 +0100

    Full support of Android 8+. (17)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoNotification.java
@@ -108,8 +108,8 @@ class ImportantInfoNotification {
             nTitle = context.getString(R.string.app_name);
             nText = title+": "+text;
         }
-        PPApplication.createInformationNotificationChannel(context);
-        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(context, PPApplication.INFORMATION_NOTIFICATION_CHANNEL)
+        PPApplication.createExclamationNotificationChannel(context);
+        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(context, PPApplication.EXCLAMATION_NOTIFICATION_CHANNEL)
                 .setColor(ContextCompat.getColor(context, R.color.primary))
                 .setSmallIcon(R.drawable.ic_exclamation_notify) // notification icon
                 .setContentTitle(nTitle) // title for notification
