1668581712d1231d7dcfc5436d13a2e9c241914f
henrichg <henrich.gron@chello.sk>
2018-04-25 18:42:59 +0200

    Execute interactive profile preferences after force stop applications. (5)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivateProfileHelper.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivateProfileHelper.java
@@ -2098,7 +2098,7 @@ class ActivateProfileHelper {
             }
         }
 
-        if (profile._deviceForceStopApplicationChange == 1)
+        if ((profile._deviceForceStopApplicationChange == 1) && AccessibilityServiceBroadcastReceiver.isEnabled(context))
         {
             // executeForInteractivePreferences() is called from broadcast receiver AccessibilityServiceBroadcastReceiver
             ActivateProfileHelper.executeForForceStopApplications(profile, context);
