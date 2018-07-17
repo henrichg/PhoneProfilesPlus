253fbf53d3e5d83c737c74dc20b61080ea60550d
henrichg <henrich.gron@chello.sk>
2018-07-17 14:30:17 +0200

    Optimized imports.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ImportantInfoActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
