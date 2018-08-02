3315d3e4cf5449529a17a0b87413939bd164536c
henrichg <henrich.gron@chello.sk>
2018-08-02 19:58:17 +0200

    Optimized imports.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventPreferencesNestedFragment.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventPreferencesNestedFragment.java
@@ -18,7 +18,6 @@ import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 
 public class EventPreferencesNestedFragment extends PreferenceFragment
                                         implements SharedPreferences.OnSharedPreferenceChangeListener
@@ -703,9 +702,9 @@ public class EventPreferencesNestedFragment extends PreferenceFragment
                     String tagName = data.getStringExtra(NFCTagWriteActivity.EXTRA_TAG_NAME);
                     //String tagUid = data.getStringExtra(NFCTagWriteActivity.EXTRA_TAG_UID);
                     long tagDbId = data.getLongExtra(NFCTagWriteActivity.EXTRA_TAG_DB_ID, 0);
-                    Log.e("EventPreferencesNestedFragment.doOnActivityResult", "tagName=" + tagName);
+                    //Log.e("EventPreferencesNestedFragment.doOnActivityResult", "tagName=" + tagName);
                     //Log.e("EventPreferencesNestedFragment.doOnActivityResult", "tagUid=" + tagUid);
-                    Log.e("EventPreferencesNestedFragment.doOnActivityResult", "tagDbId=" + tagDbId);
+                    //Log.e("EventPreferencesNestedFragment.doOnActivityResult", "tagDbId=" + tagDbId);
                     preference.setNFCTagFromEditor(tagName, "", tagDbId);
                 }
             }
