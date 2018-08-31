6c508b9d89232e419de60e39f89021b47909a7d6
henrichg <henrich.gron@chello.sk>
2018-08-31 09:14:31 +0200

    Fixed ANR:   at sk.henrichg.phoneprofilesplus.VolumeDialogPreference.<init> (VolumeDialogPreference.java:103)              at android.preference.PreferenceFragment.addPreferencesFromResource (PreferenceFragment.java:301)              at sk.henrichg.phoneprofilesplus.ProfilePreferencesFragment.addPreferencesFromResource (ProfilePreferencesFragment.java:33)              at sk.henrichg.phoneprofilesplus.PreferenceFragment.onCreate (PreferenceFragment.java:88)              at sk.henrichg.phoneprofilesplus.ProfilePreferencesNestedFragment.onCreate (ProfilePreferencesNestedFragment.java:64)              at sk.henrichg.phoneprofilesplus.ProfilePreferencesFragment.onCreate (ProfilePreferencesFragment.java:18)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/VolumeDialogPreference.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/VolumeDialogPreference.java
@@ -99,17 +99,25 @@ public class VolumeDialogPreference extends
             defaultValueVoice = audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
         }
 
-        try {
-            mediaPlayer = MediaPlayer.create(context, R.raw.volume_change_notif);
-            if (mediaPlayer != null)
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        } catch (Exception ignored) {}
-
         typedArray.recycle();
     }
 
     @Override
     protected void showDialog(Bundle state) {
+
+        PPApplication.startHandlerThreadPlayTone();
+        final Handler handler = new Handler(PPApplication.handlerThreadPlayTone.getLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mediaPlayer = MediaPlayer.create(_context, R.raw.volume_change_notif);
+                    if (mediaPlayer != null)
+                        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                } catch (Exception ignored) {}
+            }
+        });
+
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
         dialogBuilder.setTitle(getDialogTitle());
         dialogBuilder.setIcon(getDialogIcon());
