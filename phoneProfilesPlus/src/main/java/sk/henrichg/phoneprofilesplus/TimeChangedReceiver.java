9ddc8cb542f5da74e8b9b9469dc7a3f1d646181f
henrichg <henrich.gron@chello.sk>
2018-09-18 21:47:22 +0200

    Used action.equals(Intent.ACTION_TIME_CHANGED) in TimeChangedReceiver only when auto_time is disabled.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/TimeChangedReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/TimeChangedReceiver.java
@@ -3,6 +3,7 @@ package sk.henrichg.phoneprofilesplus;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 
 public class TimeChangedReceiver extends BroadcastReceiver {
     public TimeChangedReceiver() {
@@ -12,8 +13,8 @@ public class TimeChangedReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if ((intent != null) && (intent.getAction() != null)) {
             String action = intent.getAction();
-            if (action.equals(Intent.ACTION_TIMEZONE_CHANGED)/* ||
-                    action.equals(Intent.ACTION_TIME_CHANGED)*/) {
+            if (action.equals(Intent.ACTION_TIMEZONE_CHANGED) ||
+                    action.equals(Intent.ACTION_TIME_CHANGED)) {
                 PPApplication.logE("##### TimeChangedReceiver.onReceive", "xxx");
                 CallsCounter.logCounter(context, "TimeChangedReceiver.onReceive", "TimeChangedReceiver_onReceive");
 
@@ -22,17 +23,29 @@ public class TimeChangedReceiver extends BroadcastReceiver {
                 if (!PPApplication.getApplicationStarted(appContext, true))
                     return;
 
-                if ((android.os.Build.VERSION.SDK_INT >= 21) &&
-                        ApplicationPreferences.applicationUseAlarmClock(context)) {
-                    ProfileDurationAlarmBroadcastReceiver.removeAlarm(context);
-                    Profile.setActivatedProfileForDuration(context, 0);
+                boolean timeChanged = true;
+
+                if (action.equals(Intent.ACTION_TIME_CHANGED)) {
+                    timeChanged = false;
+                    String isAutoTime = Settings.Global.getString(appContext.getContentResolver(), Settings.Global.AUTO_TIME);
+                    if (isAutoTime.equals("0")) {
+                        timeChanged = true;
+                    }
                 }
 
-                SearchCalendarEventsJob.scheduleJob(/*appContext, */true, null, true);
+                if (timeChanged) {
+                    if ((android.os.Build.VERSION.SDK_INT >= 21) &&
+                            ApplicationPreferences.applicationUseAlarmClock(context)) {
+                        ProfileDurationAlarmBroadcastReceiver.removeAlarm(context);
+                        Profile.setActivatedProfileForDuration(context, 0);
+                    }
 
-                DataWrapper dataWrapper = new DataWrapper(appContext, false, 0);
-                dataWrapper.clearSensorsStartTime();
-                dataWrapper.restartEvents(false, true/*, false*/, false, true);
+                    SearchCalendarEventsJob.scheduleJob(/*appContext, */true, null, true);
+
+                    DataWrapper dataWrapper = new DataWrapper(appContext, false, 0);
+                    dataWrapper.clearSensorsStartTime();
+                    dataWrapper.restartEvents(false, true/*, false*/, false, true);
+                }
             }
         }
     }
