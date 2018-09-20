001f3dcf36f080bd89e33bb88b9ef8374f9369f3
henrichg <henrich.gron@chello.sk>
2018-09-20 20:15:28 +0200

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -64,7 +64,7 @@ public class PPApplication extends Application {
     static final int VERSION_CODE_EXTENDER_LATEST = VERSION_CODE_EXTENDER_2_0;
 
     private static final boolean logIntoLogCat = true;
-    private static final boolean logIntoFile = true;
+    private static final boolean logIntoFile = false;
     private static final boolean rootToolsDebug = false;
     private static final String logFilterTags = "##### PPApplication.onCreate"
                                          +"|PhoneProfilesService.onCreate"
@@ -170,10 +170,10 @@ public class PPApplication extends Application {
                                          //+"|[GeoSensor] DataWrapper.doHandleEvents"
 
                                          //+"|$$$B WifiBluetoothScanner"
-                                         +"|$$$W WifiBluetoothScanner"
+                                         //+"|$$$W WifiBluetoothScanner"
 
-                                         +"|WifiScanJob"
-                                         +"|WifiScanBroadcastReceiver.onReceive"
+                                         //+"|WifiScanJob"
+                                         //+"|WifiScanBroadcastReceiver.onReceive"
                                          //+"|----- DataWrapper.doHandleEvents"
 
 
