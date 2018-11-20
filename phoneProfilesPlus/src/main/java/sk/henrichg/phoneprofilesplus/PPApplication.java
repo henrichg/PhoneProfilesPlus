56e24cdff831f21e30305b7f33596411a265ceb9
henrichg <henrich.gron@chello.sk>
2018-11-20 16:26:33 +0100

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -64,7 +64,7 @@ public class PPApplication extends Application {
     //static final int VERSION_CODE_EXTENDER_3_0 = 200;
     static final int VERSION_CODE_EXTENDER_LATEST = VERSION_CODE_EXTENDER_2_0;
 
-    private static final boolean logIntoLogCat = true;
+    private static final boolean logIntoLogCat = false;
     private static final boolean logIntoFile = false;
     private static final boolean rootToolsDebug = false;
     private static final String logFilterTags = "##### PPApplication.onCreate"
@@ -271,7 +271,7 @@ public class PPApplication extends Application {
                                         //+"|EditorProfilesActivity.changeEventOrder"
                                         //+"|EditorProfilesActivity.selectDrawerItem"
 
-                                        +"|NFCTagPreference.showEditMenu"
+                                        //+"|NFCTagPreference.showEditMenu"
             ;
 
 
