4051b843304397112a28056763f7d8b46d2d6579
henrichg <henrich.gron@chello.sk>
2019-01-22 19:44:28 +0100

    Added intents support into Run applications/shortcut. (14); Changed color of info and exclamation colors in preferences. (2).

---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ApplicationEditorIntentActivity.java
@@ -0,0 +1,13 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApplicationEditorIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_editor_intent);
+    }
+}
