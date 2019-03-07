3763fc570fc863a0343f26fdcc2671be39ab7498
henrichg <henrich.gron@chello.sk>
2019-03-07 16:11:02 +0100

    Set mobile data without reflections for Android 9. (1)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CmdMobileData.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CmdMobileData.java
@@ -1,21 +1,25 @@
 package sk.henrichg.phoneprofilesplus;
 
-import android.net.IConnectivityManager;
 import android.os.ServiceManager;
+
+import com.android.internal.telephony.ITelephony;
 
 @SuppressWarnings("WeakerAccess")
 public class CmdMobileData {
 
     public static void main(String[] args) {
-        if (!(run())) {
+        int subId= Integer.parseInt(args[0]);
+        boolean enable = Boolean.parseBoolean(args[1]);
+        if (!(run(subId, enable))) {
             System.exit(1);
         }
     }
 
-    private static boolean run() {
+    private static boolean run(int subId, boolean enable) {
         try {
-            IConnectivityManager adapter = IConnectivityManager.Stub.asInterface(ServiceManager.getService("connectivity"));
-            //adapter.;
+            ITelephony adapter = ITelephony.Stub.asInterface(ServiceManager.getService("phone"));
+            //adapter.getDataEnabled(subId); // hm podpora pre dual sim
+            adapter.setUserDataEnabled(subId, enable); // hm podpora pre dual sim
             return true;
         } catch (Throwable e) {
             return false;
