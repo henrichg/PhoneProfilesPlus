05011f376b4914c9fafe44ba36fa25295d7de8eb
henrichg <henrich.gron@chello.sk>
2019-03-13 12:54:27 +0100

    Reverted back Wi-Fi hotspot for Android 9. Required is targetAPI < 28!

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivateProfileHelper.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivateProfileHelper.java
@@ -184,7 +184,7 @@ class ActivateProfileHelper {
                         }
                     }
                 }
-                /*else {
+                else {
                     boolean setWifiAPState = false;
                     boolean isWifiAPEnabled = CmdWifiAP.isEnabled();
                     switch (profile._deviceWiFiAP) {
@@ -214,7 +214,7 @@ class ActivateProfileHelper {
                         //SystemClock.sleep(200);
                         PPApplication.sleep(200);
                     }
-                }*/
+                }
             }
         }
 
