233c3052bdacd875c40e378dc933c6fb8761abb3
henrichg <henrich.gron@chello.sk>
2019-05-01 13:01:22 +0200

    Fixed usage of Crashlytics in DEBUG app version.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.util.Pair;
 
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
 import com.evernote.android.job.JobApi;
 import com.evernote.android.job.JobConfig;
 import com.evernote.android.job.JobManager;
@@ -596,12 +595,17 @@ public class PPApplication extends Application {
             // Obtain the FirebaseAnalytics instance.
             //mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+            /*
             // Set up Crashlytics, disabled for debug builds
             Crashlytics crashlyticsKit = new Crashlytics.Builder()
                     .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
                     .build();
 
             Fabric.with(this, crashlyticsKit);
+            */
+            if (!BuildConfig.DEBUG) {
+                Fabric.with(this, new Crashlytics());
+            }
             // Crashlytics.getInstance().core.logException(exception); -- this log will be associated with crash log.
         } catch (Exception e) {
             /*
