e405096436542bb721523f2d5ad9cee946335cb2
henrichg <henrich.gron@chello.sk>
2019-05-05 14:49:22 +0200

    Changed android-job from local module into library. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -698,7 +698,7 @@ public class PPApplication extends Application {
         brightnessHandler = new Handler(getMainLooper());
         screenTimeoutHandler = new Handler(getMainLooper());
 
-        JobConfig.setForceAllowApi14(true); // https://github.com/evernote/android-job/issues/197
+        //JobConfig.setForceAllowApi14(true); // https://github.com/evernote/android-job/issues/197
         JobConfig.setApiEnabled(JobApi.GCM, false); // is only important for Android 4.X
 
         JobManager.create(this).addJobCreator(new PPJobsCreator());
