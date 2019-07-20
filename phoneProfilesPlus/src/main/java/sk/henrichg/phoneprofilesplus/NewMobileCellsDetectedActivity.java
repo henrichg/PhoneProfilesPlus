93690ae541a01c4860072e903e81d5d18ac08dd3
henrichg <henrich.gron@chello.sk>
2019-07-20 20:46:30 +0200

    Added internal autoregistration of mobile cells. (6)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NewMobileCellsDetectedActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NewMobileCellsDetectedActivity.java
@@ -1,13 +1,21 @@
 package sk.henrichg.phoneprofilesplus;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
@@ -15,24 +23,70 @@ import androidx.viewpager2.widget.ViewPager2;
 
 public class NewMobileCellsDetectedActivity extends AppCompatActivity {
 
+    AlertDialog mDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        GlobalGUIRoutines.setTheme(this, false, false/*, false*/); // must by called before super.onCreate()
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
+
+        PPApplication.logE("NewMobileCellsDetectedActivity.onCreate", "xxx");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        // set theme and language for dialog alert ;-)
+        // not working on Android 2.3.x
+        GlobalGUIRoutines.setTheme(this, true, false/*, false*/);
         GlobalGUIRoutines.setLanguage(this);
 
-        super.onCreate(savedInstanceState);
+        PPApplication.logE("NewMobileCellsDetectedActivity.onStart", "xxx");
 
-        /*
-        setContentView(R.layout.activity_new_mobile_cells_detected);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle(R.string.new_mobile_cells_detected_title);
+        dialogBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle(R.string.title_activity_new_mobile_cells_detected);
-            getSupportActionBar().setElevation(GlobalGUIRoutines.dpToPx(1));
-        }
-        */
+                NewMobileCellsDetectedActivity.this.finish();
+            }
+        });
+        dialogBuilder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                NewMobileCellsDetectedActivity.this.finish();
+            }
+        });
 
+        LayoutInflater inflater = getLayoutInflater();
+        @SuppressLint("InflateParams")
+        View layout = inflater.inflate(R.layout.activity_new_mobile_cells_detected, null);
+        dialogBuilder.setView(layout);
+
+        mDialog = dialogBuilder.create();
+
+        mDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialog) {
+                NewMobileCellsDetectedActivity.this.onShow();
+            }
+        });
+
+        if (!isFinishing())
+            mDialog.show();
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(0, 0);
+    }
+
+    private void onShow() {
     }
 
 }
