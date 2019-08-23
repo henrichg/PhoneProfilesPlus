e7417359a3e99611de476917f693b726315e84bd
henrichg <henrich.gron@chello.sk>
2019-08-23 11:53:21 +0200

    Do not disable GCM in android_job.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -800,7 +800,7 @@ public class PPApplication extends Application {
 
         JobConfig.setApiEnabled(JobApi.WORK_MANAGER, true);
         //JobConfig.setForceAllowApi14(true); // https://github.com/evernote/android-job/issues/197
-        JobConfig.setApiEnabled(JobApi.GCM, false); // is only important for Android 4.X
+        //JobConfig.setApiEnabled(JobApi.GCM, false); // is only important for Android 4.X
 
         JobManager.create(this).addJobCreator(new PPJobsCreator());
 
