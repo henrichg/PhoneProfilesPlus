a01947ef797be3129e3bc0d2fff971739ebfa735
henrichg <henrich.gron@chello.sk>
2019-10-11 13:50:39 +0200

    Fixed FC: java.lang.IllegalArgumentException: regist too many Broadcast Receivers.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -22,6 +22,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
+import com.llew.huawei.verifier.LoadedApkHuaWei;
 import com.samsung.android.sdk.SsdkUnsupportedException;
 import com.samsung.android.sdk.look.Slook;
 import com.stericson.RootShell.RootShell;
@@ -724,6 +725,9 @@ public class PPApplication extends Application {
             }
         }
         //////////////////////////////////////////
+
+        // Fix for FC: java.lang.IllegalArgumentException: register too many Broadcast Receivers
+        LoadedApkHuaWei.hookHuaWeiVerifier(this);
 
         /*
         if (logIntoFile || crashIntoFile)
