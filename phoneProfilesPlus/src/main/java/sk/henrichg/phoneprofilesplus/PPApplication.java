4e51023b1e8e491eda803d102c995659e3bdf7a9
henrichg <henrich.gron@chello.sk>
2019-10-15 19:54:46 +0200

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -75,7 +75,7 @@ public class PPApplication extends Application {
 
     @SuppressWarnings("PointlessBooleanExpression")
     private static final boolean logIntoLogCat = true && BuildConfig.DEBUG;
-    static final boolean logIntoFile = false;
+    static final boolean logIntoFile = true;
     @SuppressWarnings("PointlessBooleanExpression")
     static final boolean crashIntoFile = true && BuildConfig.DEBUG;
     private static final boolean rootToolsDebug = false;
@@ -275,6 +275,9 @@ public class PPApplication extends Application {
                                          //+"|[GeoSensor] DataWrapper.doHandleEvents"
                                          //+"|[***] GeofenceScanner"
                                          //+"|GeofenceScanWorker"
+                                         +"|GeofenceScanner.updateTransitionsByLastKnownLocation"
+                                         +"|GeofenceScanWorker.doWork"
+                                         +"|GeofenceScanner.LocationCallback"
 
                                          //+"|WifiStateChangedBroadcastReceiver"
                                          //+"|WifiConnectionBroadcastReceiver"
@@ -484,7 +487,7 @@ public class PPApplication extends Application {
 
                                         //+"|[BRS] SettingsContentObserver.onChange"
                                         //+"|----- ActivateProfileHelper.execute"
-                                        +"|BluetoothNamePreferenceFragmentX"
+                                        //+"|BluetoothNamePreferenceFragmentX"
             ;
 
 
