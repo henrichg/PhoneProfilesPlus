0d25e86feec96d2da2c3cc640197b5d042037edc
henrichg <henrich.gron@chello.sk>
2019-11-09 22:34:04 +0100

    Added show/hide animation to GuiInfoPopupWindow. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GuiInfoPopupWindow.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GuiInfoPopupWindow.java
@@ -35,13 +35,6 @@ class GuiInfoPopupWindow extends RelativePopupWindow {
         applyDim(root);
 
         /*
-        // Disable default animation for circular reveal
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setAnimationStyle(0);
-        }
-        */
-
-        /*
         popupView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -87,49 +80,5 @@ class GuiInfoPopupWindow extends RelativePopupWindow {
         ViewGroupOverlay overlay = parent.getOverlay();
         overlay.clear();
     }
-
-    /*
-    @Override
-    public void showOnAnchor(@NonNull View anchor, int verticalPos, int horizontalPos, int x, int y) {
-        super.showOnAnchor(anchor, verticalPos, horizontalPos, x, y);
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        //    circularReveal(anchor);
-        //}
-    }
-    */
-
-    /*
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void circularReveal(@NonNull final View anchor) {
-        final View contentView = getContentView();
-        contentView.post(new Runnable() {
-            @Override
-            public void run() {
-                if (ViewCompat.isAttachedToWindow(anchor)) {
-                //if (anchor.isAttachedToWindow()) {
-                    try {
-                        final int[] myLocation = new int[2];
-                        final int[] anchorLocation = new int[2];
-                        //contentView.getLocationOnScreen(myLocation);
-                        contentView.getLocationInWindow(myLocation);
-                        //anchor.getLocationOnScreen(anchorLocation);
-                        anchor.getLocationInWindow(anchorLocation);
-                        final int cx = anchorLocation[0] - myLocation[0] + anchor.getWidth() / 2;
-                        final int cy = anchorLocation[1] - myLocation[1] + anchor.getHeight() / 2;
-
-                        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-                        final int dx = Math.max(cx, contentView.getMeasuredWidth() - cx);
-                        final int dy = Math.max(cy, contentView.getMeasuredHeight() - cy);
-                        final float finalRadius = (float) Math.hypot(dx, dy);
-                        Animator animator = ViewAnimationUtils.createCircularReveal(contentView, cx, cy, 0f, finalRadius);
-                        animator.setDuration(500);
-                        animator.start();
-                    } catch (Exception ignored) {}
-                }
-            }
-        });
-    }
-    */
-
 
 }
