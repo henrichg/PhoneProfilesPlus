6e5b0d160357a7d94642e7a4aacca51ba64e5901
henrichg <henrich.gron@chello.sk>
2020-01-03 15:44:12 +0100

    Test of brightness for OnePlus, Android 10.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -83,7 +83,7 @@ public class PPApplication extends Application {
 
     @SuppressWarnings("PointlessBooleanExpression")
     private static final boolean logIntoLogCat = true && BuildConfig.DEBUG;
-    static final boolean logIntoFile = false;
+    static final boolean logIntoFile = true;
     @SuppressWarnings("PointlessBooleanExpression")
     static final boolean crashIntoFile = true && BuildConfig.DEBUG;
     private static final boolean rootToolsDebug = false;
@@ -522,7 +522,7 @@ public class PPApplication extends Application {
                                         //+"|Profile.convertPercentsToBrightnessManualValue"
                                         //+"|Profile.convertPercentsToBrightnessAdaptiveValue"
                                         //+"|Profile.convertBrightnessToPercents"
-                                        //+"|[BRS] SettingsContentObserver.onChange"
+                                        +"|[BRS] SettingsContentObserver.onChange"
 
                                         //+"|EditorProfileListFragment.refreshGUI"
                                         //+"|EditorEventListFragment.refreshGUI"
