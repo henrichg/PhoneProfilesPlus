21248130b7c3437311167419b7c8dfa6c280b6b2
henrichg <henrich.gron@chello.sk>
2020-02-10 09:23:34 +0100

    Maybe fixed non-fatal exception: Exception: java.io.IOException: setDataSource failed.                                         at android.media.MediaPlayer.setDataSource(MediaPlayer.java:1187)                                         at android.media.MediaPlayer.setDataSource(MediaPlayer.java:1160)                                         at android.media.MediaPlayer.setDataSource(MediaPlayer.java:1078)                                         at android.media.MediaPlayer.setDataSource(MediaPlayer.java:1003)                                         at sk.henrichg.phoneprofilesplus.RingtonePreferenceX$3.run(RingtonePreferenceX.java:332).

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/RingtonePreferenceX.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/RingtonePreferenceX.java
@@ -300,10 +300,14 @@ public class RingtonePreferenceX extends DialogPreference {
     }
 
     void playRingtone() {
+        if ((ringtoneUri == null) || ringtoneUri.isEmpty())
+            return;
+
         final AudioManager audioManager = (AudioManager)prefContext.getSystemService(Context.AUDIO_SERVICE);
         if (audioManager != null) {
 
             final Uri _ringtoneUri = Uri.parse(ringtoneUri);
+            Crashlytics.log("RingtonePreferenceX.playRingtone - ringtoneUri="+ringtoneUri);
 
             PPApplication.startHandlerThreadPlayTone();
             final Handler handler = new Handler(PPApplication.handlerThreadPlayTone.getLooper());
