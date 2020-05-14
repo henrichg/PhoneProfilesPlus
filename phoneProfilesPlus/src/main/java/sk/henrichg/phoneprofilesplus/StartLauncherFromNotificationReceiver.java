29f0e6b5584b8b55e90b8413c2c8a673ef9b1200
henrichg <henrich.gron@gmail.com>
2020-05-14 15:44:39 +0200

    Fixed display of Activator from notification.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/StartLauncherFromNotificationReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/StartLauncherFromNotificationReceiver.java
@@ -3,6 +3,7 @@ package sk.henrichg.phoneprofilesplus;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 
 // Disable action button
 public class StartLauncherFromNotificationReceiver extends BroadcastReceiver {
@@ -15,16 +16,27 @@ public class StartLauncherFromNotificationReceiver extends BroadcastReceiver {
 
         if (intent != null) {
             String action = intent.getAction();
-            if ((action != null) && action.equals(PhoneProfilesService.ACTION_START_LAUNCHER_FROM_NOTIFICATION)) {
-                Context appContext = context.getApplicationContext();
-                // intent to LauncherActivity, for click on notification
-                Intent launcherIntent = new Intent(appContext, LauncherActivity.class);
-                // clear all opened activities
-                launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK/*|Intent.FLAG_ACTIVITY_NO_ANIMATION*/);
-                // setup startupSource
-                launcherIntent.putExtra(PPApplication.EXTRA_STARTUP_SOURCE, PPApplication.STARTUP_SOURCE_NOTIFICATION);
-                appContext.startActivity(launcherIntent);
+            if (action != null) {
+                //PPApplication.logE("StartLauncherFromNotificationReceiver.onReceive", "action="+action);
+
+                if (action.equals(PhoneProfilesService.ACTION_START_LAUNCHER_FROM_NOTIFICATION)) {
+                    final Context appContext = context.getApplicationContext();
+                    Handler _handler = new Handler(appContext.getMainLooper());
+                    Runnable r = new Runnable() {
+                        public void run() {
+                            // intent to LauncherActivity, for click on notification
+                            Intent launcherIntent = new Intent(appContext, LauncherActivity.class);
+                            // clear all opened activities
+                            launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK/*|Intent.FLAG_ACTIVITY_NO_ANIMATION*/);
+                            // setup startupSource
+                            launcherIntent.putExtra(PPApplication.EXTRA_STARTUP_SOURCE, PPApplication.STARTUP_SOURCE_NOTIFICATION);
+                            appContext.startActivity(launcherIntent);
+                        }
+                    };
+                    _handler.postDelayed(r, 500);
+                }
             }
         }
     }
+
 }
