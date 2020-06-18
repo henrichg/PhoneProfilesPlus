15f7d5595072bcb47579d54b5b86ebbd541fd883
henrichg <henrich.gron@gmail.com>
2020-06-18 21:42:45 +0200

    Added set of custom color in color dialog for profile icons.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileIconColorChooserDialogX.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileIconColorChooserDialogX.java
@@ -2,6 +2,7 @@ package sk.henrichg.phoneprofilesplus;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
@@ -12,9 +13,15 @@ import android.graphics.drawable.StateListDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.gridlayout.widget.GridLayout;
+
+import com.kunzisoft.androidclearchroma.IndicatorMode;
+import com.kunzisoft.androidclearchroma.colormode.ColorMode;
+import com.kunzisoft.androidclearchroma.view.ChromaColorView;
 
 class ProfileIconColorChooserDialogX implements View.OnClickListener {
 
@@ -64,29 +71,80 @@ class ProfileIconColorChooserDialogX implements View.OnClickListener {
 
         this.defaultColor = ProfileIconPreferenceAdapterX.getIconColor(preference.imageIdentifier/*, prefContext*/);
 
+        // default icon color --------------------------------
+
         final FrameLayout defaultColorLayout = layout.findViewById(R.id.dialog_color_chooser_default_color);
 
         defaultColorLayout.setTag(-1);
         defaultColorLayout.setOnClickListener(this);
-        defaultColorLayout.getChildAt(0).setVisibility(preselect == -1 ? View.VISIBLE : View.GONE);
+
+        ImageView check = (ImageView) defaultColorLayout.getChildAt(0);
+
+        if(Color.red(profileIconPreference.customColor) +
+            Color.green(profileIconPreference.customColor) +
+            Color.blue(profileIconPreference.customColor) < 300)
+            check.setImageResource(R.drawable.ic_check);
+        else
+            check.setImageResource(R.drawable.ic_check_dark);
+
+        check.setVisibility((!preference.useCustomColor) && (preselect == -1) ? View.VISIBLE : View.GONE);
 
         Drawable selector = createSelector(defaultColor);
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            int[][] states = new int[][]{
-                    new int[]{-android.R.attr.state_pressed},
-                    new int[]{android.R.attr.state_pressed}
-            };
-            int[] colors = new int[]{
-                    shiftColor(defaultColor),
-                    defaultColor
-            };
-            ColorStateList rippleColors = new ColorStateList(states, colors);
-            setBackgroundCompat(defaultColorLayout, new RippleDrawable(rippleColors, selector, null));
-        //} else {
-        //    setBackgroundCompat(defaultColorLayout, selector);
-        //}
+        int[][] states = new int[][]{
+                new int[]{-android.R.attr.state_pressed},
+                new int[]{android.R.attr.state_pressed}
+        };
+        int[] colors = new int[]{
+                shiftColor(defaultColor),
+                defaultColor
+        };
+        ColorStateList rippleColors = new ColorStateList(states, colors);
+        setBackgroundCompat(defaultColorLayout, new RippleDrawable(rippleColors, selector, null));
 
+        final TextView defaultColorLabel = layout.findViewById(R.id.dialog_color_chooser_default_color_label);
+        defaultColorLabel.setTag(-1);
+        defaultColorLabel.setOnClickListener(this);
 
+        //--------------------------------------
+        // custom icon color -------------------
+
+        // use custom color but it is not any from preselected
+        if (preference.useCustomColor && (preselect == -1))
+            preselect = -2;
+
+        final FrameLayout customColorLayout = layout.findViewById(R.id.dialog_color_chooser_custom_color);
+
+        customColorLayout.setTag(-2);
+        customColorLayout.setOnClickListener(this);
+
+        check = (ImageView) customColorLayout.getChildAt(0);
+
+        if(Color.red(profileIconPreference.customColor) +
+            Color.green(profileIconPreference.customColor) +
+            Color.blue(profileIconPreference.customColor) < 300)
+            check.setImageResource(R.drawable.ic_check);
+        else
+            check.setImageResource(R.drawable.ic_check_dark);
+
+        check.setVisibility(preselect == -2 ? View.VISIBLE : View.GONE);
+
+        selector = createSelector(profileIconPreference.customColor);
+        states = new int[][]{
+                new int[]{-android.R.attr.state_pressed},
+                new int[]{android.R.attr.state_pressed}
+        };
+        colors = new int[]{
+                shiftColor(profileIconPreference.customColor),
+                profileIconPreference.customColor
+        };
+        rippleColors = new ColorStateList(states, colors);
+        setBackgroundCompat(customColorLayout, new RippleDrawable(rippleColors, selector, null));
+
+        final TextView customColorLabel = layout.findViewById(R.id.dialog_color_chooser_custom_color_label);
+        customColorLabel.setTag(-2);
+        customColorLabel.setOnClickListener(this);
+
+        //--------------------------------------
 
         final GridLayout list = layout.findViewById(R.id.dialog_color_chooser_grid);
 
@@ -97,20 +155,16 @@ class ProfileIconColorChooserDialogX implements View.OnClickListener {
             child.getChildAt(0).setVisibility(preselect == i ? View.VISIBLE : View.GONE);
 
             selector = createSelector(mColors[i]);
-            //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                /*int[][]*/ states = new int[][]{
-                        new int[]{-android.R.attr.state_pressed},
-                        new int[]{android.R.attr.state_pressed}
-                };
-                /*int[]*/ colors = new int[]{
-                        shiftColor(mColors[i]),
-                        mColors[i]
-                };
-                /*ColorStateList*/ rippleColors = new ColorStateList(states, colors);
-                setBackgroundCompat(child, new RippleDrawable(rippleColors, selector, null));
-            //} else {
-            //    setBackgroundCompat(child, selector);
-            //}
+            states = new int[][]{
+                    new int[]{-android.R.attr.state_pressed},
+                    new int[]{android.R.attr.state_pressed}
+            };
+            colors = new int[]{
+                    shiftColor(mColors[i]),
+                    mColors[i]
+            };
+            rippleColors = new ColorStateList(states, colors);
+            setBackgroundCompat(child, new RippleDrawable(rippleColors, selector, null));
         }
 
     }
@@ -119,11 +173,44 @@ class ProfileIconColorChooserDialogX implements View.OnClickListener {
     public void onClick(View v) {
         if (v.getTag() != null) {
             Integer index = (Integer) v.getTag();
-            int color = defaultColor;
-            if (index > -1)
-                color = mColors[index];
-            profileIconPreference.setCustomColor(index > -1, color);
-            mDialog.dismiss();
+            if (index == -2) {
+                // custom color
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                dialogBuilder.setTitle(R.string.colorChooser_pref_dialog_title);
+                dialogBuilder.setCancelable(true);
+                dialogBuilder.setNegativeButton(android.R.string.cancel, null);
+
+                LayoutInflater inflater = activity.getLayoutInflater();
+                @SuppressLint("InflateParams")
+                View layout = inflater.inflate(R.layout.dialog_custom_color_preference, null);
+                dialogBuilder.setView(layout);
+
+                final ChromaColorView chromaColorView = layout.findViewById(R.id.custom_color_chroma_color_view);
+                //PPApplication.logE("CustomColorDialogPreferenceFragmentX.onBindDialogView", "preference.value="+preference.value);
+                chromaColorView.setCurrentColor(profileIconPreference.customColor);
+                chromaColorView.setColorMode(ColorMode.values()[0]);
+                chromaColorView.setIndicatorMode(IndicatorMode.values()[1]);
+
+                dialogBuilder.setPositiveButton(R.string.alert_button_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        int color = chromaColorView.getCurrentColor();
+                        profileIconPreference.setCustomColor(true, color);
+                        mDialog.dismiss();
+                    }
+                });
+                dialogBuilder.setNegativeButton(R.string.alert_button_no, null);
+
+                AlertDialog dialog = dialogBuilder.create();
+                if (!activity.isFinishing())
+                    dialog.show();
+            }
+            else {
+                int color = defaultColor;
+                if (index > -1)
+                    color = mColors[index];
+                profileIconPreference.setCustomColor(index > -1, color);
+                mDialog.dismiss();
+            }
         }
     }
 
