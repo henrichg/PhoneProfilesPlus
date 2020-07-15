2a0e512a33c12cb3ce0e0e1bc78b639efefce4a0
henrichg <henrich.gron@gmail.com>
2020-07-15 11:20:23 +0200

    Maybe fixed battery consumption and block of PPHandlerThread due to contacts change. (2)

---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ContactsContentObserverWorker.java
@@ -0,0 +1,39 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class ContactsContentObserverWorker extends Worker {
+
+    final Context context;
+
+    static final String WORK_TAG = "contactsContentObserverWork";
+
+    public ContactsContentObserverWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters params) {
+        super(context, params);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        try {
+            Context appContext = context.getApplicationContext();
+
+            // must be first
+            PhoneProfilesService.createContactsCache(appContext, true);
+            //must be seconds, this ads groups int contacts
+            PhoneProfilesService.createContactGroupsCache(appContext, true);
+
+            return Result.success();
+        } catch (Exception e) {
+            PPApplication.recordException(e);
+            return Result.failure();
+        }
+    }
+}
