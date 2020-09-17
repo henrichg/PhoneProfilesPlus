4c0efdb76240720506a369eda5ca9865db48fdc7
henrichg <henrich.gron@gmail.com>
2020-09-17 09:47:02 +0200

    Fixed FC: java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.String android.content.Context.getPackageName()' on a null object reference                  at android.content.ComponentName.<init>(:3)                  at android.content.Intent.<init>(:8)                  at sk.henrichg.phoneprofilesplus.PhoneProfilesDashClockExtension$1.run(PhoneProfilesDashClockExtension.java:108).

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesDashClockExtension.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PhoneProfilesDashClockExtension.java
@@ -60,6 +60,9 @@ public class PhoneProfilesDashClockExtension extends DashClockExtension {
         if (dataWrapper == null)
             return;
 
+        final PhoneProfilesDashClockExtension _instance = instance;
+        final DataWrapper _dataWrapper = dataWrapper;
+
         PPApplication.startHandlerThreadWidget();
         final Handler handler = new Handler(PPApplication.handlerThreadWidget.getLooper());
         handler.post(new Runnable() {
@@ -67,75 +70,67 @@ public class PhoneProfilesDashClockExtension extends DashClockExtension {
             public void run() {
 //                PPApplication.logE("[HANDLER CALL] PPApplication.startHandlerThreadWidget", "START run - from=PhoneProfilesDashClockExtension.onUpdateData");
 
-                if (instance == null)
-                    return;
+                try {
+//                    PPApplication.logE("[HANDLER CALL] PhoneProfilesDashClockExtension.onUpdateData", "do it");
 
-                if (dataWrapper == null)
-                    return;
+                    //profile = Profile.getMappedProfile(
+                    //                            _dataWrapper.getActivatedProfile(true, false), this);
+                    Profile profile = _dataWrapper.getActivatedProfile(true, false);
 
-//                PPApplication.logE("[HANDLER CALL] PhoneProfilesDashClockExtension.onUpdateData", "do it");
-
-                //profile = Profile.getMappedProfile(
-                //                            dataWrapper.getActivatedProfile(true, false), this);
-                Profile profile = dataWrapper.getActivatedProfile(true, false);
-
-                boolean isIconResourceID;
-                String iconIdentifier;
-                String profileName;
-                if (profile != null)
-                {
-                    isIconResourceID = profile.getIsIconResourceID();
-                    iconIdentifier = profile.getIconIdentifier();
-                    profileName = DataWrapper.getProfileNameWithManualIndicatorAsString(profile, true, "", false, false, false, dataWrapper);
-                }
-                else
-                {
-                    isIconResourceID = true;
-                    iconIdentifier = Profile.PROFILE_ICON_DEFAULT;
-                    profileName = getResources().getString(R.string.profiles_header_profile_name_no_activated);
-                }
-                int iconResource;
-                if (isIconResourceID)
-                    //iconResource = getResources().getIdentifier(iconIdentifier, "drawable", PPApplication.PACKAGE_NAME);
-                    iconResource = Profile.getIconResource(iconIdentifier);
-                else
-                    //iconResource = getResources().getIdentifier(Profile.PROFILE_ICON_DEFAULT, "drawable", PPApplication.PACKAGE_NAME);
-                    iconResource = Profile.getIconResource(Profile.PROFILE_ICON_DEFAULT);
-
-                /////////////////////////////////////////////////////////////
-
-                // intent
-                Intent intent = new Intent(instance, LauncherActivity.class);
-                // clear all opened activities
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(PPApplication.EXTRA_STARTUP_SOURCE, PPApplication.STARTUP_SOURCE_WIDGET);
-
-                String status = "";
-                if (ApplicationPreferences.prefEventsBlocked)
-                    if (ApplicationPreferences.prefForceRunEventRunning)
-                    {
-                /*if (android.os.Build.VERSION.SDK_INT >= 16)
-                    status = "\u23E9";
-                else*/
-                        status = "[»]";
+                    boolean isIconResourceID;
+                    String iconIdentifier;
+                    String profileName;
+                    if (profile != null) {
+                        isIconResourceID = profile.getIsIconResourceID();
+                        iconIdentifier = profile.getIconIdentifier();
+                        profileName = DataWrapper.getProfileNameWithManualIndicatorAsString(profile, true, "", false, false, false, _dataWrapper);
+                    } else {
+                        isIconResourceID = true;
+                        iconIdentifier = Profile.PROFILE_ICON_DEFAULT;
+                        profileName = getResources().getString(R.string.profiles_header_profile_name_no_activated);
                     }
+                    int iconResource;
+                    if (isIconResourceID)
+                        //iconResource = getResources().getIdentifier(iconIdentifier, "drawable", PPApplication.PACKAGE_NAME);
+                        iconResource = Profile.getIconResource(iconIdentifier);
                     else
-                    {
-                /*if (android.os.Build.VERSION.SDK_INT >= 16)
-                    status = "\uD83D\uDC46";
-                else */
-                        status = "[M]";
-                    }
+                        //iconResource = getResources().getIdentifier(Profile.PROFILE_ICON_DEFAULT, "drawable", PPApplication.PACKAGE_NAME);
+                        iconResource = Profile.getIconResource(Profile.PROFILE_ICON_DEFAULT);
 
-                // Publish the extension data update.
-                publishUpdate(new ExtensionData()
-                        .visible(true)
-                        .icon(iconResource)
-                        .status(status)
-                        .expandedTitle(profileName)
-                        .expandedBody(ProfilePreferencesIndicator.getString(profile, 25, instance))
-                        .contentDescription("PhoneProfilesPlus - "+profileName)
-                        .clickIntent(intent));
+                    /////////////////////////////////////////////////////////////
+
+                    // intent
+                    Intent intent = new Intent(_instance, LauncherActivity.class);
+                    // clear all opened activities
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra(PPApplication.EXTRA_STARTUP_SOURCE, PPApplication.STARTUP_SOURCE_WIDGET);
+
+                    String status = "";
+                    if (ApplicationPreferences.prefEventsBlocked)
+                        if (ApplicationPreferences.prefForceRunEventRunning) {
+                        /*if (android.os.Build.VERSION.SDK_INT >= 16)
+                            status = "\u23E9";
+                        else*/
+                            status = "[»]";
+                        } else {
+                        /*if (android.os.Build.VERSION.SDK_INT >= 16)
+                            status = "\uD83D\uDC46";
+                        else */
+                            status = "[M]";
+                        }
+
+                    // Publish the extension data update.
+                    publishUpdate(new ExtensionData()
+                            .visible(true)
+                            .icon(iconResource)
+                            .status(status)
+                            .expandedTitle(profileName)
+                            .expandedBody(ProfilePreferencesIndicator.getString(profile, 25, _instance))
+                            .contentDescription("PhoneProfilesPlus - " + profileName)
+                            .clickIntent(intent));
+                } catch (Exception e) {
+                    PPApplication.recordException(e);
+                }
             }
         });
     }
