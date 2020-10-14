789b57300dfcab4a5b28283210b14caec6e45b57
henrichg <henrich.gron@gmail.com>
2020-10-14 23:36:12 +0200

    Added button with GitHub "Assets" screenshot for PPP and PPPE installation. (3)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GitHubAssetsScreenshotActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/GitHubAssetsScreenshotActivity.java
@@ -1,7 +1,9 @@
 package sk.henrichg.phoneprofilesplus;
 
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +22,32 @@ public class GitHubAssetsScreenshotActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_github_assets_screenshot);
-        setTitle("GitHub \"Assets\" " + getString(R.string.github_assets_screenshot_label));
+        setTaskDescription(new ActivityManager.TaskDescription(getString(R.string.ppp_app_name)));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("GitHub \"Assets\" " + getString(R.string.github_assets_screenshot_label));
+            getSupportActionBar().setElevation(0/*GlobalGUIRoutines.dpToPx(1)*/);
+        }
 
         ImageView imageView = findViewById(R.id.github_assets_screenshot_activity_image);
         imageView.setContentDescription("GitHub \"Assets\" " + getString(R.string.github_assets_screenshot_label));
         int image = getIntent().getIntExtra(EXTRA_IMAGE, R.drawable.ic_empty);
         imageView.setImageResource(image);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            finish();
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
