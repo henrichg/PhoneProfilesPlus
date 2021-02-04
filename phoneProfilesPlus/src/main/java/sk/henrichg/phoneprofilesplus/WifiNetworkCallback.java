267988758ca56b9d246efc1f15febe8d04b49000
Henrich Gron <henrich.gron@gmail.com>
2021-02-04 03:51:45 +0100

    Fixed start of PPP, activation of Default profile. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/WifiNetworkCallback.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/WifiNetworkCallback.java
@@ -29,7 +29,6 @@ public class WifiNetworkCallback extends ConnectivityManager.NetworkCallback {
     @Override
     public void onUnavailable() {
 //        PPApplication.logE("[IN_LISTENER] ----------- WifiNetworkCallback.onUnavailable", "xxx");
-        //connected = false;
         doConnection();
     }
 
