7cd71d4cfb2c442e851011fdaf5be55b35b36e4c
Henrich Gron <henrich.gron@gmail.com>
2021-02-21 16:11:07 +0100

    New Extender version. In it is fixed FC. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -84,7 +84,6 @@ public class PPApplication extends Application
     static final int VERSION_CODE_EXTENDER_3_0 = 200;
     static final int VERSION_CODE_EXTENDER_4_0 = 400;
     static final int VERSION_CODE_EXTENDER_5_1_3_1 = 540;
-    static final int VERSION_CODE_EXTENDER_5_1_4 = 590;
     static final int VERSION_CODE_EXTENDER_5_1_4_1 = 600;
     static final int VERSION_CODE_EXTENDER_LATEST = VERSION_CODE_EXTENDER_5_1_4_1;
 
