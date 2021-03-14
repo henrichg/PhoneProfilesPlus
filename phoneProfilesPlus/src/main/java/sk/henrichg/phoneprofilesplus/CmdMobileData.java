d33ae06f991ca0519aef3f4329dafb214ece09a2
Henrich Gron <henrich.gron@gmail.com>
2021-03-15 00:42:42 +0100

    Better check of mobile datat on/off for single sim card mobiles.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CmdMobileData.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CmdMobileData.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.ServiceManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 
 import com.android.internal.telephony.ITelephony;
 
@@ -38,47 +39,52 @@ public class CmdMobileData {
                 boolean ok = false;
                 ITelephony adapter = ITelephony.Stub.asInterface(ServiceManager.getService("phone")); // service list | grep ITelephony
                 if (adapter != null) {
-                    SubscriptionManager mSubscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
-                    //SubscriptionManager.from(context);
-                    if (mSubscriptionManager != null) {
-                        int defaultDataId = 0;
-                        if (Build.VERSION.SDK_INT > 23)
-                            defaultDataId = SubscriptionManager.getDefaultDataSubscriptionId();
-//                        PPApplication.logE("CmdMobileData.isEnabled", "defaultDataId=" + defaultDataId);
+                    if ((Build.VERSION.SDK_INT > 23) && (simCard > 0)) {
+                        SubscriptionManager mSubscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+                        //SubscriptionManager.from(context);
+                        if (mSubscriptionManager != null) {
+                            int defaultDataId = 0;
+                            //noinspection ConstantConditions
+                            if (Build.VERSION.SDK_INT > 23)
+                                defaultDataId = SubscriptionManager.getDefaultDataSubscriptionId();
+//                            PPApplication.logE("CmdMobileData.isEnabled", "defaultDataId=" + defaultDataId);
 
-                        List<SubscriptionInfo> subscriptionList = null;
-                        try {
-                            // Loop through the subscription list i.e. SIM list.
-                            subscriptionList = mSubscriptionManager.getActiveSubscriptionInfoList();
-                        } catch (SecurityException e) {
-                            PPApplication.recordException(e);
-                        }
-                        if (subscriptionList != null) {
-                            for (int i = 0; i < subscriptionList.size();/*mSubscriptionManager.getActiveSubscriptionInfoCountMax();*/ i++) {
-                                // Get the active subscription ID for a given SIM card.
-                                SubscriptionInfo subscriptionInfo = subscriptionList.get(i);
-                                if (subscriptionInfo != null) {
-                                    int slotIndex = subscriptionInfo.getSimSlotIndex();
-                                    if ((simCard == 0) || (simCard == (slotIndex + 1))) {
-                                        int subscriptionId = subscriptionInfo.getSubscriptionId();
-                                        if (Build.VERSION.SDK_INT > 23)
+                            List<SubscriptionInfo> subscriptionList = null;
+                            try {
+                                // Loop through the subscription list i.e. SIM list.
+                                subscriptionList = mSubscriptionManager.getActiveSubscriptionInfoList();
+                            } catch (SecurityException e) {
+                                PPApplication.recordException(e);
+                            }
+                            if (subscriptionList != null) {
+                                for (int i = 0; i < subscriptionList.size();/*mSubscriptionManager.getActiveSubscriptionInfoCountMax();*/ i++) {
+                                    // Get the active subscription ID for a given SIM card.
+                                    SubscriptionInfo subscriptionInfo = subscriptionList.get(i);
+                                    if (subscriptionInfo != null) {
+                                        int slotIndex = subscriptionInfo.getSimSlotIndex();
+                                        if (simCard == (slotIndex + 1)) {
+                                            int subscriptionId = subscriptionInfo.getSubscriptionId();
                                             enabled = adapter.getDataEnabled(subscriptionId) && (subscriptionId == defaultDataId);
-                                        else
-                                            enabled = adapter.getDataEnabled(subscriptionId);
-//                                        PPApplication.logE("CmdMobileData.isEnabled", "subscriptionId=" + subscriptionId);
-//                                        PPApplication.logE("CmdMobileData.isEnabled", "simCard=" + simCard);
-//                                        PPApplication.logE("CmdMobileData.isEnabled", "slotIndex=" + (slotIndex + 1));
-//                                        PPApplication.logE("CmdMobileData.isEnabled", "enabled=" + enabled);
-                                        ok = true;
+//                                            PPApplication.logE("CmdMobileData.isEnabled", "subscriptionId=" + subscriptionId);
+//                                            PPApplication.logE("CmdMobileData.isEnabled", "simCard=" + simCard);
+//                                            PPApplication.logE("CmdMobileData.isEnabled", "slotIndex=" + (slotIndex + 1));
+//                                            PPApplication.logE("CmdMobileData.isEnabled", "enabled=" + enabled);
+                                            ok = true;
+                                        }
+                                        if (ok)
+                                            break;
                                     }
-                                    if (ok)
-                                        break;
                                 }
                             }
                         }
+                        if (!ok) {
+                            int dataState = adapter.getDataState();
+                            enabled = dataState == TelephonyManager.DATA_CONNECTED;
+                        }
                     }
-                    if (!ok) {
-                        enabled = adapter.getDataEnabled(1);
+                    else {
+                        int dataState = adapter.getDataState();
+                        enabled = dataState == TelephonyManager.DATA_CONNECTED;
                     }
                 }
             }
