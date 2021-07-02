24bce3c9b2826982fea82736602145eef7cc17c0
Henrich Gron <henrich.gron@gmail.com>
2021-07-02 21:00:35 +0200

    Added Amazon Appstore for check releases. (5) Updated translations.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CheckGitHubReleasesActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/CheckGitHubReleasesActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Spannable;
