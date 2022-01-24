95148543d2d26644ad3ee1a6dc3b370d116f6962
henrichg <henrich.gron@gmail.com>
2022-01-24 09:06:25 +0100

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -152,7 +152,7 @@ public class PPApplication extends Application
     @SuppressWarnings("PointlessBooleanExpression")
     private static final boolean logIntoLogCat = true && DebugVersion.enabled;
     //TODO change it back to not log crash for releases
-    static final boolean logIntoFile = false;
+    static final boolean logIntoFile = true;
     @SuppressWarnings("PointlessBooleanExpression")
     static final boolean crashIntoFile = false && DebugVersion.enabled;
     private static final boolean rootToolsDebug = false;
