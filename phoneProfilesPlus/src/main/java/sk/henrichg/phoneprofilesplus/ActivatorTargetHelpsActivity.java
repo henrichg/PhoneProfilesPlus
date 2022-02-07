f3884380ecfad06c337b2fde99882b3f85babe0b
henrichg <henrich.gron@gmail.com>
2022-02-07 12:45:12 +0100

    Fixed TapTargetHelps status bar and navigation bar color for Activator.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivatorTargetHelpsActivity.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ActivatorTargetHelpsActivity.java
@@ -1,7 +1,11 @@
 package sk.henrichg.phoneprofilesplus;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -15,6 +19,14 @@ public class ActivatorTargetHelpsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(0, 0);
+
+        Window window = getWindow();
+
+        View decorView = window.getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.setNavigationBarColor(Color.TRANSPARENT);
 
         activity = this;
     }
