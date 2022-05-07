525fb8d6eee59a8c2001a2d0f8b9db8c6423aba9
henrichg <henrich.gron@gmail.com>
2022-05-07 13:26:51 +0200

    ...

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventTimeBroadcastReceiver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventTimeBroadcastReceiver.java
@@ -16,41 +16,36 @@ public class EventTimeBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
         if (action != null) {
             //PPApplication.logE("EventTimeBroadcastReceiver.onReceive", "action=" + action);
-            doReceiver(/*true,*/ context);
-        }
-    }
 
-    private void doReceiver(/*boolean useHandler,*/ Context context) {
+            if (!PPApplication.getApplicationStarted(true))
+                // application is not started
+                return;
 
-        if (!PPApplication.getApplicationStarted(true))
-            // application is not started
-            return;
+            if (Event.getGlobalEventsRunning()) {
+                //if (useHandler) {
+                final Context appContext = context.getApplicationContext();
+                PPApplication.startHandlerThreadBroadcast(/*"EventTimeBroadcastReceiver.onReceive"*/);
+                final Handler __handler = new Handler(PPApplication.handlerThreadBroadcast.getLooper());
+                //__handler.post(new PPApplication.PPHandlerThreadRunnable(
+                //        context.getApplicationContext()) {
+                __handler.post(() -> {
+//                    PPApplication.logE("[IN_THREAD_HANDLER] PPApplication.startHandlerThread", "START run - from=EventTimeBroadcastReceiver.onReceive");
 
-        if (Event.getGlobalEventsRunning()) {
-            //if (useHandler) {
-            final Context appContext = context.getApplicationContext();
-            PPApplication.startHandlerThreadBroadcast(/*"EventTimeBroadcastReceiver.doWork"*/);
-            final Handler __handler = new Handler(PPApplication.handlerThreadBroadcast.getLooper());
-            //__handler.post(new PPApplication.PPHandlerThreadRunnable(
-            //        context.getApplicationContext()) {
-            __handler.post(() -> {
-//                    PPApplication.logE("[IN_THREAD_HANDLER] PPApplication.startHandlerThread", "START run - from=EventTimeBroadcastReceiver.doWork");
-
-                //Context appContext= appContextWeakRef.get();
-                //if (appContext != null) {
+                    //Context appContext= appContextWeakRef.get();
+                    //if (appContext != null) {
                     PowerManager powerManager = (PowerManager) appContext.getSystemService(Context.POWER_SERVICE);
                     PowerManager.WakeLock wakeLock = null;
                     try {
                         if (powerManager != null) {
-                            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, PPApplication.PACKAGE_NAME + ":EventTimeBroadcastReceiver_doWork");
+                            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, PPApplication.PACKAGE_NAME + ":EventTimeBroadcastReceiver_onReceive");
                             wakeLock.acquire(10 * 60 * 1000);
                         }
 
-//                        PPApplication.logE("[EVENTS_HANDLER_CALL] EventTimeBroadcastReceiver.doWork", "sensorType=SENSOR_TYPE_TIME");
+//                        PPApplication.logE("[EVENTS_HANDLER_CALL] EventTimeBroadcastReceiver.onReceive", "sensorType=SENSOR_TYPE_TIME");
                         EventsHandler eventsHandler = new EventsHandler(appContext);
                         eventsHandler.handleEvents(EventsHandler.SENSOR_TYPE_TIME);
 
-                        //PPApplication.logE("****** EventsHandler.handleEvents", "END run - from=EventTimeBroadcastReceiver.doWork");
+                        //PPApplication.logE("****** EventsHandler.handleEvents", "END run - from=EventTimeBroadcastReceiver.onReceive");
                     } catch (Exception e) {
 //                        PPApplication.logE("[IN_THREAD_HANDLER] PPApplication.startHandlerThread", Log.getStackTraceString(e));
                         PPApplication.recordException(e);
@@ -62,16 +57,18 @@ public class EventTimeBroadcastReceiver extends BroadcastReceiver {
                             }
                         }
                     }
-                //}
-            });
+                    //}
+                });
             /*}
             else {
                 if (Event.getGlobalEventsRunning(appContext)) {
-                    PPApplication.logE("EventTimeBroadcastReceiver.doWork", "handle events");
+                    PPApplication.logE("EventTimeBroadcastReceiver.onReceive", "handle events");
                     EventsHandler eventsHandler = new EventsHandler(appContext);
                     eventsHandler.handleEvents(EventsHandler.SENSOR_TYPE_TIME);
                 }
             }*/
+            }
+
         }
     }
 
