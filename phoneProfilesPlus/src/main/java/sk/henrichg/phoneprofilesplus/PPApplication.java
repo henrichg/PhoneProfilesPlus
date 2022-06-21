7437f739639cebce8aaa8d753da05c8128448b38
henrichg <henrich.gron@gmail.com>
2022-06-21 11:04:01 +0200

    Change: new PPPE version as required.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -92,8 +92,9 @@ public class PPApplication extends Application
     //static final int VERSION_CODE_EXTENDER_6_0 = 620;
     //static final int VERSION_CODE_EXTENDER_6_2 = 670;
     //static final int VERSION_CODE_EXTENDER_7_0 = 700;
-    static final int VERSION_CODE_EXTENDER_LATEST = 700;
-    static final String VERSION_NAME_EXTENDER_LATEST = "7.0";
+    //static final int VERSION_CODE_EXTENDER_8_0 = 800;
+    static final int VERSION_CODE_EXTENDER_LATEST = 800;
+    static final String VERSION_NAME_EXTENDER_LATEST = "8.0";
 
     static final int pid = Process.myPid();
     static final int uid = Process.myUid();
