bee4afd1f0811b97d6ba2c7a59ee4f1403dc2796
henrichg <henrich.gron@gmail.com>
2022-06-23 20:56:46 +0200

    Change: better preferences indicator color in notification and widgets. (7)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfilePreferencesIndicator.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfilePreferencesIndicator.java
@@ -262,10 +262,15 @@ class ProfilePreferencesIndicator {
                     ColorMatrix saturationCM = new ColorMatrix();
                     saturationCM.setSaturation(0);
                     paint.setColorFilter(new ColorMatrixColorFilter(saturationCM));
-                    if (nightModeOn)
-                        paint.setAlpha(DISABLED_ALPHA_DYNAMIC_DARK);
-                    else
-                        paint.setAlpha(DISABLED_ALPHA_DYNAMIC_LIGHT);
+                    if (indicatorsType != DataWrapper.IT_FOR_WIDGET_NATIVE_BACKGROUND) {
+                        // for IT_FOR_WIDGET_NATIVE_BACKGROUND is forced background color
+                        // set alpha is not needed
+
+                        if (nightModeOn)
+                            paint.setAlpha(DISABLED_ALPHA_DYNAMIC_DARK);
+                        else
+                            paint.setAlpha(DISABLED_ALPHA_DYNAMIC_LIGHT);
+                    }
                 } else
                     paint.setAlpha(DISABLED_ALPHA_MONOCHROME);
             }
