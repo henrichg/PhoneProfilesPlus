7cbd030ef53a21af832bab60ea877aa26ba18111
henrichg <henrich.gron@gmail.com>
2022-07-11 13:30:59 +0200

    Added "VPN" parameter into profile preferences. (1)

---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/VPNDialogPreferenceFragmentX.java
@@ -0,0 +1,136 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceDialogFragmentCompat;
+
+public class VPNDialogPreferenceFragmentX extends PreferenceDialogFragmentCompat
+        implements AdapterView.OnItemSelectedListener
+{
+
+    private Context context;
+    private VPNDialogPreferenceX preference;
+
+    private AppCompatSpinner vpnApplicationSpinner = null;
+    private EditText profileNameEditText = null;
+    private EditText tunnelNameEditText = null;
+
+    @SuppressLint("InflateParams")
+    @Override
+    protected View onCreateDialogView(@NonNull Context context)
+    {
+        this.context = context;
+        preference = (VPNDialogPreferenceX) getPreference();
+        preference.fragment = this;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return inflater.inflate(R.layout.dialog_vpn_preference, null, false);
+    }
+
+    @Override
+    protected void onBindDialogView(@NonNull View view) {
+        super.onBindDialogView(view);
+
+        vpnApplicationSpinner = view.findViewById(R.id.vpnPrefDialogVPNApplication);
+
+        GlobalGUIRoutines.HighlightedSpinnerAdapter vpnApplicationSpinnerAdapter = new GlobalGUIRoutines.HighlightedSpinnerAdapter(
+                (EventsPrefsActivity) context,
+                R.layout.highlighted_spinner,
+                getResources().getStringArray(R.array.volumesSensorOperatorArray));
+        vpnApplicationSpinnerAdapter.setDropDownViewResource(R.layout.highlighted_spinner_dropdown);
+        vpnApplicationSpinner.setAdapter(vpnApplicationSpinnerAdapter);
+        vpnApplicationSpinner.setPopupBackgroundResource(R.drawable.popupmenu_background);
+        vpnApplicationSpinner.setBackgroundTintList(ContextCompat.getColorStateList(context/*getBaseContext()*/, R.color.highlighted_spinner_all));
+
+        profileNameEditText = view.findViewById(R.id.vpnPrefDialogProfileName);
+        profileNameEditText.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.highlighted_spinner_all));
+        profileNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                preference.profileName = profileNameEditText.getText().toString();
+
+                preference.callChangeListener(preference.getSValue());
+            }
+        });
+
+        tunnelNameEditText = view.findViewById(R.id.cpnPrefDialogTunnelName);
+        tunnelNameEditText.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.highlighted_spinner_all));
+        tunnelNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                preference.tunnelName = tunnelNameEditText.getText().toString();
+
+                preference.callChangeListener(preference.getSValue());
+            }
+        });
+
+        String[] entryValues = getResources().getStringArray(R.array.volumesSensorOperatorValues);
+        int vpnApplicationIdx = 0;
+        for (String entryValue : entryValues) {
+            if (entryValue.equals(String.valueOf(preference.vpnApplication))) {
+                break;
+            }
+            ++vpnApplicationIdx;
+        }
+        vpnApplicationSpinner.setSelection(vpnApplicationIdx);
+
+        profileNameEditText.setText(preference.profileName);
+        tunnelNameEditText.setText(preference.tunnelName);
+
+        vpnApplicationSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onDialogClosed(boolean positiveResult) {
+        if (positiveResult) {
+            preference.persistValue();
+        }
+        else {
+            preference.resetSummary();
+        }
+
+        preference.fragment = null;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        ((GlobalGUIRoutines.HighlightedSpinnerAdapter)vpnApplicationSpinner.getAdapter()).setSelection(position);
+
+        String[] vpnApplicationValues = context.getResources().getStringArray(R.array.vpnApplicationValues);
+        preference.vpnApplication = Integer.parseInt(vpnApplicationValues[position]);
+
+        preference.callChangeListener(preference.getSValue());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+}
---
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/VPNDialogPreferenceX.java
@@ -0,0 +1,203 @@
+package sk.henrichg.phoneprofilesplus;
+
+import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.AttributeSet;
+
+import androidx.preference.DialogPreference;
+
+public class VPNDialogPreferenceX extends DialogPreference {
+
+    VPNDialogPreferenceFragmentX fragment;
+
+    private final Context _context;
+
+    private String sValue = "0||";
+    private String defaultValue;
+    private boolean savedInstanceState;
+
+    int vpnApplication;
+    String profileName;
+    String tunnelName;
+
+    public VPNDialogPreferenceX(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        _context = context;
+
+    }
+
+    @Override
+    protected void onSetInitialValue(Object defaultValue)
+    {
+        // Get the persistent value and correct it for the minimum value.
+        sValue = getPersistedString((String) defaultValue);
+        this.defaultValue = (String)defaultValue;
+        //PPApplication.logE("VolumeDialogPreferenceX.getValueVDP", "form onSetInitialValue");
+        getValueVDP();
+        setSummaryVDP();
+    }
+
+    private void getValueVDP()
+    {
+        //PPApplication.logE("VolumeDialogPreferenceX.getValueVDP", "sValue="+sValue);
+        String[] splits = sValue.split("\\|");
+        try {
+            vpnApplication = Integer.parseInt(splits[0]);
+            profileName = splits[1];
+            tunnelName = splits[2];
+        } catch (Exception e) {
+            //Log.e("VolumeDialogPreferenceX.getValueVDP", Log.getStackTraceString(e));
+            PPApplication.recordException(e);
+            vpnApplication = 0;
+            profileName = "";
+            tunnelName = "";
+        }
+    }
+
+    private void setSummaryVDP()
+    {
+
+        String prefVolumeDataSummary;
+        String[] entries = _context.getResources().getStringArray(R.array.vpnApplicationArray);
+        String[] entryValues = _context.getResources().getStringArray(R.array.vpnApplicationValues);
+
+        int applicaitonIdx = 0;
+        for (String entryValue : entryValues) {
+            if (entryValue.equals(String.valueOf(vpnApplication))) {
+                break;
+            }
+            ++applicaitonIdx;
+        }
+
+        prefVolumeDataSummary = entries[applicaitonIdx];
+
+        if (vpnApplication == 0)
+            prefVolumeDataSummary = prefVolumeDataSummary + " " + profileName;
+        else
+            prefVolumeDataSummary = prefVolumeDataSummary + " " + tunnelName;
+
+        setSummary(prefVolumeDataSummary);
+    }
+
+    String getSValue() {
+        return vpnApplication
+                + "|" + profileName
+                + "|" + tunnelName;
+    }
+
+    void persistValue() {
+        if (shouldPersist()) {
+            persistString(getSValue());
+            setSummaryVDP();
+        }
+    }
+
+    void resetSummary() {
+        if (!savedInstanceState) {
+            sValue = getPersistedString(defaultValue);
+            getValueVDP();
+            setSummaryVDP();
+        }
+        savedInstanceState = false;
+    }
+
+    /*
+    static boolean changeEnabled(String value) {
+        String[] splits = value.split("\\|");
+        if (splits.length > 1) {
+            try {
+                return Integer.parseInt(splits[1]) == 0;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        else
+            return false;
+    }
+    */
+
+    @Override
+    protected Parcelable onSaveInstanceState()
+    {
+        savedInstanceState = true;
+
+        final Parcelable superState = super.onSaveInstanceState();
+        /*if (isPersistent()) {
+            return superState;
+        }*/
+
+        final VPNDialogPreferenceX.SavedState myState = new VPNDialogPreferenceX.SavedState(superState);
+        myState.sValue = getSValue();
+        myState.defaultValue = defaultValue;
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state)
+    {
+        if ((state == null) || (!state.getClass().equals(VPNDialogPreferenceX.SavedState.class))) {
+            // Didn't save state for us in onSaveInstanceState
+            super.onRestoreInstanceState(state);
+            //PPApplication.logE("VolumeDialogPreferenceX.getValueVDP", "form onRestoreInstanceState");
+            getValueVDP();
+            setSummaryVDP();
+            return;
+        }
+
+        // restore instance state
+        VPNDialogPreferenceX.SavedState myState = (VPNDialogPreferenceX.SavedState)state;
+        super.onRestoreInstanceState(myState.getSuperState());
+        sValue = myState.sValue;
+        defaultValue = myState.defaultValue;
+
+        //PPApplication.logE("VolumeDialogPreferenceX.getValueVDP", "form onRestoreInstanceState");
+        getValueVDP();
+        setSummaryVDP();
+    }
+
+    // SavedState class
+    private static class SavedState extends BaseSavedState
+    {
+        String sValue;
+        String defaultValue;
+
+        SavedState(Parcel source)
+        {
+            super(source);
+
+            sValue = source.readString();
+            defaultValue = source.readString();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags)
+        {
+            super.writeToParcel(dest, flags);
+
+            dest.writeString(sValue);
+            dest.writeString(defaultValue);
+        }
+
+        SavedState(Parcelable superState)
+        {
+            super(superState);
+        }
+
+        public static final Creator<SavedState> CREATOR =
+                new Creator<VPNDialogPreferenceX.SavedState>() {
+                    public VPNDialogPreferenceX.SavedState createFromParcel(Parcel in)
+                    {
+                        return new VPNDialogPreferenceX.SavedState(in);
+                    }
+                    public VPNDialogPreferenceX.SavedState[] newArray(int size)
+                    {
+                        return new VPNDialogPreferenceX.SavedState[size];
+                    }
+
+                };
+
+    }
+
+}
