bfe114b32338f4d04537b15413636d5df14a628f
henrichg <henrich.gron@gmail.com>
2022-08-13 14:32:06 +0200

    Fixed bad detected sim card 0. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventsHandler.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventsHandler.java
@@ -145,6 +145,7 @@ class EventsHandler {
     static final int SENSOR_TYPE_ACTIVATED_PROFILE = 49;
     static final int SENSOR_TYPE_ROAMING = 50;
     static final int SENSOR_TYPE_VPN = 51;
+    static final int SENSOR_TYPE_SIM_STATE_CHANGED = 52;
     static final int SENSOR_TYPE_ALL = 999;
 
     public EventsHandler(Context context) {
@@ -782,6 +783,7 @@ class EventsHandler {
             case SENSOR_TYPE_EVENT_DELAY_START:
             case SENSOR_TYPE_EVENT_DELAY_END:
             case SENSOR_TYPE_DEVICE_IDLE_MODE:
+            case SENSOR_TYPE_SIM_STATE_CHANGED:
                 return true;
         }
         return false;
