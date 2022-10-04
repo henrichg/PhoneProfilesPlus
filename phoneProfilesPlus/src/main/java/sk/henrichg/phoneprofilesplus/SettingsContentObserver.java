a6cc8274fee293dff8fb9a6fe3d3accedf5d426e
henrichg <henrich.gron@gmail.com>
2022-10-04 18:54:49 +0200

    Updated translations.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/SettingsContentObserver.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/SettingsContentObserver.java
@@ -317,6 +317,9 @@ class SettingsContentObserver  extends ContentObserver {
             savedBrightness = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, -1);
             savedAdaptiveBrightness = Settings.System.getFloat(context.getContentResolver(), Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, -1);
 
+            // TODO this is for log brightness values to log file
+            //  use only for check brightness values 0%, 50%, 100% by user,
+            //  when in his device brightness not working good
 //            PPApplication.logE("SettingsContentObserver.onChange", "savedBrightnessMode="+savedBrightnessMode);
 //            PPApplication.logE("SettingsContentObserver.onChange", "savedBrightness="+savedBrightness);
 //            PPApplication.logE("SettingsContentObserver.onChange", "savedAdaptiveBrightness="+savedAdaptiveBrightness);
