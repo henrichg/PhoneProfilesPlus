9636afa36456fdc7e5fb07f37cbd30da842fa599
henrichg <henrich.gron@gmail.com>
2022-11-28 21:58:41 +0100

    Change: Changed selector for selected profile icon in profile icon preference dialog.

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileIconPreferenceAdapter.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ProfileIconPreferenceAdapter.java
@@ -40,6 +40,7 @@ class ProfileIconPreferenceAdapter extends BaseAdapter {
 
     static class ViewHolder {
           ImageView icon;
+        View mark;
           //int position;
         }
 
@@ -53,6 +54,7 @@ class ProfileIconPreferenceAdapter extends BaseAdapter {
             vi = LayoutInflater.from(context).inflate(R.layout.profileicon_preference_gridview_item, parent, false);
             holder = new ViewHolder();
             holder.icon = vi.findViewById(R.id.profileicon_preference_gridview_item_icon);
+            holder.mark = vi.findViewById(R.id.profileicon_preference_gridview_item_mark);
             vi.setTag(holder);
         }
         else
@@ -64,17 +66,19 @@ class ProfileIconPreferenceAdapter extends BaseAdapter {
         String iconResName = ProfileStatic.getImageResourceName(position);
         if (iconResName.equals(preference.imageIdentifier) && preference.isImageResourceID) {
             //if (Build.VERSION.SDK_INT >= 21)
-                //holder.icon.setBackgroundColor(GlobalGUIRoutines.getThemeColorControlHighlight(context));
-                holder.icon.setBackgroundResource(R.drawable.profile_icon_background);
+            //holder.icon.setBackgroundColor(GlobalGUIRoutines.getThemeColorControlHighlight(context));
+            //    holder.icon.setBackgroundResource(R.drawable.profile_icon_background);
             /*else {
                 if (ApplicationPreferences.applicationTheme(context, true).equals("dark"))
                     holder.icon.setBackgroundResource(R.drawable.abc_list_selector_background_transition_holo_dark);
                 else
                     holder.icon.setBackgroundResource(R.drawable.abc_list_selector_background_transition_holo_light);
             }*/
+            holder.mark.setVisibility(View.VISIBLE);
+        } else {
+            //holder.icon.setBackgroundResource(0);
+            holder.mark.setVisibility(View.INVISIBLE);
         }
-        else
-            holder.icon.setBackgroundResource(0);
 
         int iconRes = Profile.profileIconId[position];
         /*if (iconResName.equals(preference.imageIdentifier) && preference.isImageResourceID && preference.useCustomColor) {
