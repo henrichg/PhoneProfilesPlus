f80bc338b352ffee772e33b5001fbb8a3f2b18be
henrichg <henrich.gron@gmail.com>
2023-08-12 19:20:56 +0200

    Change: Slow RelativeLayout into LinearLayout. (5)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/AskForDurationDialog.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/AskForDurationDialog.java
@@ -10,7 +10,6 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EditorEventListFragment.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EditorEventListFragment.java
@@ -25,7 +25,6 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
