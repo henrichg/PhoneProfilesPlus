a1728985be7bae15fc39cb846e892805673bc460
henrichg <henrich.gron@gmail.com>
2023-10-03 10:51:48 +0200

    Change: Maybe fixed call, sms, notification sensor, when contacts are synchronized. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -175,7 +175,7 @@ public class PPApplication extends Application
     @SuppressWarnings("PointlessBooleanExpression")
     static final boolean logIntoLogCat = true && DebugVersion.enabled;
     //TODO change it back to not log crash for releases
-    static final boolean logIntoFile = false;
+    static final boolean logIntoFile = true;
     @SuppressWarnings("PointlessBooleanExpression")
     static final boolean crashIntoFile = false && DebugVersion.enabled;
     static final boolean rootToolsDebug = false;
