696ee76f9b284f4bdc1845eb880a1091086ff7c3
henrichg <henrich.gron@gmail.com>
2023-10-05 09:19:20 +0200

    Change: Maybe fixed call, sms, notification sensor, when contacts are synchronized. (11)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ContactGroupsMultiSelectDialogPreferenceFragment.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/ContactGroupsMultiSelectDialogPreferenceFragment.java
@@ -142,7 +142,7 @@ public class ContactGroupsMultiSelectDialogPreferenceFragment extends Preference
                 ContactsCache contactsCache = PPApplicationStatic.getContactsCache();
                 if (contactsCache == null) {
                     // cache not created, create it
-                    PPApplicationStatic.createContactsCache(prefContext.getApplicationContext(), false, false, false);
+                    PPApplicationStatic.createContactsCache(prefContext.getApplicationContext(), false, false, true);
                     /*contactsCache = PPApplicationStatic.getContactsCache();
                     while (contactsCache.getCaching())
                         GlobalUtils.sleep(100);*/
@@ -152,7 +152,7 @@ public class ContactGroupsMultiSelectDialogPreferenceFragment extends Preference
                         List<Contact> contactList = contactsCache.getList(/*withoutNumbers*/);
                         if (contactList == null) {
                             // not cached, cache it
-                            PPApplicationStatic.createContactsCache(prefContext.getApplicationContext(), false, false, false);
+                            PPApplicationStatic.createContactsCache(prefContext.getApplicationContext(), false, false, true);
                             /*contactsCache = PPApplicationStatic.getContactsCache();
                             while (contactsCache.getCaching())
                                 GlobalUtils.sleep(100);*/
@@ -167,7 +167,7 @@ public class ContactGroupsMultiSelectDialogPreferenceFragment extends Preference
                 ContactGroupsCache contactGroupsCache = PPApplicationStatic.getContactGroupsCache();
                 if (contactGroupsCache == null) {
                     // cache not created, create it
-                    PPApplicationStatic.createContactGroupsCache(prefContext.getApplicationContext(), false, false, false);
+                    PPApplicationStatic.createContactGroupsCache(prefContext.getApplicationContext(), false, false, true);
                     /*contactGroupsCache = PPApplicationStatic.getContactGroupsCache();
                     while (contactGroupsCache.getCaching())
                         GlobalUtils.sleep(100);*/
@@ -177,7 +177,7 @@ public class ContactGroupsMultiSelectDialogPreferenceFragment extends Preference
                         List<ContactGroup> contactGroupList = contactGroupsCache.getList(/*withoutNumbers*/);
                         if (contactGroupList == null) {
                             // not cached, cache it
-                            PPApplicationStatic.createContactGroupsCache(prefContext.getApplicationContext(), false, false, false);
+                            PPApplicationStatic.createContactGroupsCache(prefContext.getApplicationContext(), false, false, true);
                             /*contactGroupsCache = PPApplicationStatic.getContactGroupsCache();
                             while (contactGroupsCache.getCaching())
                                 GlobalUtils.sleep(100);*/
