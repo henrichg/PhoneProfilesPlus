36691fd9be336beecda4f17a196fa710c62f14b4
henrichg <henrich.gron@gmail.com>
2024-01-22 18:46:55 +0100

    Change: Some fixes about memory leaks. (14)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NFCTagPreferenceFragment.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/NFCTagPreferenceFragment.java
@@ -205,8 +205,12 @@ public class NFCTagPreferenceFragment extends PreferenceDialogFragmentCompat {
             preference.nfcTagList.clear();
         listAdapter.notifyDataSetChanged();
         final Handler handler = new Handler(prefContext.getMainLooper());
-        final NFCTagPreferenceFragment fragment = this;
-        handler.postDelayed(() -> fragment.refreshListView(""), 200);
+        final WeakReference<NFCTagPreferenceFragment> fragmentWeakRef = new WeakReference<>(this);
+        handler.postDelayed(() -> {
+            NFCTagPreferenceFragment fragment = fragmentWeakRef.get();
+            if (fragment != null)
+                fragment.refreshListView("");
+        }, 200);
     }
 
     @Override
