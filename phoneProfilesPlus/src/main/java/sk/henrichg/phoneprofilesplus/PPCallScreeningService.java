e959ddeaa761fc46a994899d3f5187b57b0f425a
henrichg <henrich.gron@gmail.com>
2024-05-08 21:23:53 +0200

    Add: "Phone calls" parameter for block calls. (21)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPCallScreeningService.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPCallScreeningService.java
@@ -16,60 +16,65 @@ public class PPCallScreeningService extends CallScreeningService {
     public void onScreenCall(Call.Details callDetails) {
         if (Build.VERSION.SDK_INT >= 29) {
             CallResponse.Builder response = new CallResponse.Builder();
-            //Log.e("PPCallScreeningService.onScreenCall", "Call screening service triggered");
+            if (callDetails.getCallDirection() ==  Call.Details.DIRECTION_INCOMING) {
+                //Log.e("PPCallScreeningService.onScreenCall", "Call screening service triggered");
 
-            //DataWrapper dataWrapper = new DataWrapper(getApplicationContext(), false, 0, false, 0, 0, 0f);
-            //Profile activatedProfile = dataWrapper.getActivatedProfile(false, false);
+                //DataWrapper dataWrapper = new DataWrapper(getApplicationContext(), false, 0, false, 0, 0, 0f);
+                //Profile activatedProfile = dataWrapper.getActivatedProfile(false, false);
 
-            // get profile from shared preferences, saved in Profile.execute()
-            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(PPApplication.TMP_SHARED_PREFS_ACTIVATE_PROFILE_HELPER_EXECUTE, Context.MODE_PRIVATE);
+                // get profile from shared preferences, saved in Profile.execute()
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(PPApplication.TMP_SHARED_PREFS_ACTIVATE_PROFILE_HELPER_EXECUTE, Context.MODE_PRIVATE);
 
-            String contacts = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACTS, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACTS));
-            String contactGroups = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_GROUPS, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_GROUPS));
-            int contactListType = Integer.parseInt(sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_LIST_TYPE, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_LIST_TYPE)));
-            boolean blockCalls = sharedPreferences.getBoolean(Profile.PREF_PROFILE_PHONE_CALLS_BLOCK_CALLS, Boolean.TRUE.equals(Profile.defaultValuesBoolean.get(Profile.PREF_PROFILE_PHONE_CALLS_BLOCK_CALLS)));
-            boolean sendSMS = sharedPreferences.getBoolean(Profile.PREF_PROFILE_PHONE_CALLS_SEND_SMS, Boolean.TRUE.equals(Profile.defaultValuesBoolean.get(Profile.PREF_PROFILE_PHONE_CALLS_SEND_SMS)));
-            String smsText = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_SMS_TEXT, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_SMS_TEXT));
+                String contacts = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACTS, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACTS));
+                String contactGroups = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_GROUPS, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_GROUPS));
+                int contactListType = Integer.parseInt(sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_LIST_TYPE, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_CONTACT_LIST_TYPE)));
+                boolean blockCalls = sharedPreferences.getBoolean(Profile.PREF_PROFILE_PHONE_CALLS_BLOCK_CALLS, Boolean.TRUE.equals(Profile.defaultValuesBoolean.get(Profile.PREF_PROFILE_PHONE_CALLS_BLOCK_CALLS)));
+                boolean sendSMS = sharedPreferences.getBoolean(Profile.PREF_PROFILE_PHONE_CALLS_SEND_SMS, Boolean.TRUE.equals(Profile.defaultValuesBoolean.get(Profile.PREF_PROFILE_PHONE_CALLS_SEND_SMS)));
+                String smsText = sharedPreferences.getString(Profile.PREF_PROFILE_PHONE_CALLS_SMS_TEXT, Profile.defaultValuesString.get(Profile.PREF_PROFILE_PHONE_CALLS_SMS_TEXT));
 
-            boolean profileFound = false;
-            boolean phoneNumberFound = false;
-            String calledPhoneNumber = callDetails.getHandle().getSchemeSpecificPart();
-            if (
-                 (
-                  (contactListType == EventPreferencesCall.CONTACT_LIST_TYPE_NOT_USE) ||
-                  ((contacts != null) && (!contacts.isEmpty())) ||
-                  ((contactGroups != null) && (!contactGroups.isEmpty()))
-                 ) && blockCalls
-            ) {
+                boolean profileFound = false;
+                boolean phoneNumberFound = false;
+                String calledPhoneNumber = callDetails.getHandle().getSchemeSpecificPart();
+                if (
+                        (
+                                (contactListType == EventPreferencesCall.CONTACT_LIST_TYPE_NOT_USE) ||
+                                        ((contacts != null) && (!contacts.isEmpty())) ||
+                                        ((contactGroups != null) && (!contactGroups.isEmpty()))
+                        ) && blockCalls
+                ) {
 
-                profileFound = true;
+                    profileFound = true;
 
-                ContactsCache contactsCache = PPApplicationStatic.getContactsCache();
-                if (contactsCache != null) {
-                    List<Contact> contactList;
+                    ContactsCache contactsCache = PPApplicationStatic.getContactsCache();
+                    if (contactsCache != null) {
+                        List<Contact> contactList;
 //                            PPApplicationStatic.logE("[SYNCHRONIZED] EventPreferencesCall.doHandleEvent", "PPApplication.contactsCacheMutex");
-                    synchronized (PPApplication.contactsCacheMutex) {
-                        contactList = contactsCache.getList(/*false*/);
+                        synchronized (PPApplication.contactsCacheMutex) {
+                            contactList = contactsCache.getList(/*false*/);
+                        }
+                        phoneNumberFound = isPhoneNumberConfigured(contacts, contactGroups, contactListType, contactList, calledPhoneNumber);
+                        if (contactList != null)
+                            contactList.clear();
                     }
-                    phoneNumberFound = isPhoneNumberConfigured(contacts, contactGroups, contactListType, contactList, calledPhoneNumber);
-                    if (contactList != null)
-                        contactList.clear();
                 }
-            }
 
-            if (profileFound && phoneNumberFound) {
-                response.setDisallowCall(true);
-                response.setRejectCall(true);
-                if (Permissions.checkSendSMS(getApplicationContext())) {
-                    if (sendSMS &&
-                            (smsText != null) && (!smsText.isEmpty())) {
-                        try {
-                            SmsManager smsManager = SmsManager.getDefault();
-                            smsManager.sendTextMessage(calledPhoneNumber, null, smsText, null, null);
-                        } catch (Exception e) {
-                            PPApplicationStatic.recordException(e);
+                if (profileFound && phoneNumberFound) {
+                    response.setDisallowCall(true);
+                    response.setRejectCall(true);
+                    if (Permissions.checkSendSMS(getApplicationContext())) {
+                        if (sendSMS &&
+                                (smsText != null) && (!smsText.isEmpty())) {
+                            try {
+                                SmsManager smsManager = SmsManager.getDefault();
+                                smsManager.sendTextMessage(calledPhoneNumber, null, smsText, null, null);
+                            } catch (Exception e) {
+                                PPApplicationStatic.recordException(e);
+                            }
                         }
                     }
+                } else {
+                    response.setDisallowCall(false);
+                    response.setRejectCall(false);
                 }
             } else {
                 response.setDisallowCall(false);
