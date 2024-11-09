5c7ccf08671c5233132d9565fcf10b5586d58f41
henrichg <henrich.gron@gmail.com>
2024-11-09 12:11:17 +0100

    Change: Acronyms strings. (6)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventPreferencesTime.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/EventPreferencesTime.java
@@ -261,7 +261,7 @@ class EventPreferencesTime extends EventPreferences {
                             if (_event.getStatus() == Event.ESTATUS_PAUSE) {
                                 alarmTime = computeAlarm(true/*, context*/);
                                 // date and time format by user system settings configuration
-                                alarmTimeS = "("+StringConstants.EVENT_START_TIME+") " + DateFormat.getDateFormat(context).format(alarmTime) +
+                                alarmTimeS = StringConstants.EVENT_START_TIME+" " + DateFormat.getDateFormat(context).format(alarmTime) +
                                         " " + DateFormat.getTimeFormat(context).format(alarmTime);
                                 _value.append(StringConstants.TAG_BREAK_HTML);
                                 _value.append(StringConstants.CHAR_HARD_SPACE_HTML).append(StringConstants.CHAR_HARD_SPACE_HTML).append(StringConstants.CHAR_HARD_SPACE_HTML).append("-> ").append(alarmTimeS);
@@ -308,7 +308,7 @@ class EventPreferencesTime extends EventPreferences {
                                             if (_event.getStatus() == Event.ESTATUS_PAUSE) {
                                                 alarmTime = computeAlarm(true/*, context*/);
                                                 // date and time format by user system settings configuration
-                                                alarmTimeS = "("+StringConstants.EVENT_START_TIME+") " + DateFormat.getDateFormat(context).format(alarmTime) +
+                                                alarmTimeS = StringConstants.EVENT_START_TIME+" " + DateFormat.getDateFormat(context).format(alarmTime) +
                                                         " " + DateFormat.getTimeFormat(context).format(alarmTime);
                                                 _value.append(StringConstants.TAG_BREAK_HTML);
                                                 _value.append(StringConstants.CHAR_HARD_SPACE_HTML).append(StringConstants.CHAR_HARD_SPACE_HTML).append(StringConstants.CHAR_HARD_SPACE_HTML).append("-> ").append(alarmTimeS);
