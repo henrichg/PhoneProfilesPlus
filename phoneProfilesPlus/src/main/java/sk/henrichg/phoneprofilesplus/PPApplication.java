75ba859f321e1cc66b96409e6621a58ec863309a
henrichg <henrich.gron@gmail.com>
2025-01-04 11:41:17 +0100

    Change: Increased speed of profile activation. (2)

--- phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
+++ phoneProfilesPlus/src/main/java/sk/henrichg/phoneprofilesplus/PPApplication.java
@@ -47,7 +47,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
